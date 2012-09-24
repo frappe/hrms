c405d258e56b94cf6f8429d30b36fcde92b3e694
Rushabh Mehta <rmehta@gmail.com>
2012-09-24 19:13:42 +0530

    moved directory structure

--- hrms/erpnext/hr/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import unicode_literals
-install_docs = [
-	{"doctype":"Role", "role_name":"Employee", "name":"Employee"},
-	{"doctype":"Role", "role_name":"HR Manager", "name":"HR Manager"},
-	{"doctype":"Role", "role_name":"HR User", "name":"HR User"},
-]
