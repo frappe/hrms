6f3ef1b9bd720a4d18f9669abd8eb5b0f51f7812
Rucha Mahabal <ruchamahabal2@gmail.com>
2022-07-04 16:46:49 +0530

    ci: install wkhtmltopdf before running tests

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -24,6 +24,14 @@ mysql --host 127.0.0.1 --port 3306 -u root -e "GRANT ALL PRIVILEGES ON \`test_fr
 mysql --host 127.0.0.1 --port 3306 -u root -e "UPDATE mysql.user SET Password=PASSWORD('travis') WHERE User='root'"
 mysql --host 127.0.0.1 --port 3306 -u root -e "FLUSH PRIVILEGES"
 
+install_whktml() {
+    wget -O /tmp/wkhtmltox.tar.xz https://github.com/frappe/wkhtmltopdf/raw/master/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
+    tar -xf /tmp/wkhtmltox.tar.xz -C /tmp
+    sudo mv /tmp/wkhtmltox/bin/wkhtmltopdf /usr/local/bin/wkhtmltopdf
+    sudo chmod o+x /usr/local/bin/wkhtmltopdf
+}
+install_whktml &
+
 cd ~/frappe-bench || exit
 
 sed -i 's/watch:/# watch:/g' Procfile
@@ -32,8 +40,10 @@ sed -i 's/socketio:/# socketio:/g' Procfile
 sed -i 's/redis_socketio:/# redis_socketio:/g' Procfile
 
 bench get-app https://github.com/ruchamahabal/erpnext --branch hr-separation
+bench setup requirements --dev
 
-bench start &
+bench start &> bench_run_logs.txt &
+CI=Yes bench build --app frappe &
 bench --site test_site reinstall --yes
 
 bench get-app hrms "${GITHUB_WORKSPACE}"
