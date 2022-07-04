e7ed9e1ded4178f354a972b48d0b920d17f316f2
Rucha Mahabal <ruchamahabal2@gmail.com>
2022-07-04 14:43:04 +0530

    fix: temporarily use separation branch for CI

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -31,7 +31,7 @@ sed -i 's/schedule:/# schedule:/g' Procfile
 sed -i 's/socketio:/# socketio:/g' Procfile
 sed -i 's/redis_socketio:/# redis_socketio:/g' Procfile
 
-bench get-app erpnext --branch develop
+bench get-app https://github.com/ruchamahabal/erpnext --branch hr-separation
 
 bench start &
 bench --site test_site reinstall --yes
