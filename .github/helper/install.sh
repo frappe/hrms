6b51f06902f9190f0a8a752ab8209fe8fd62571d
Rucha Mahabal <ruchamahabal2@gmail.com>
2022-07-04 14:07:09 +0530

    fix: ci

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 cd ~ || exit
 
-sudo apt-get install redis-server libcups2-dev -qq
+sudo apt-get -y install redis-server libcups2-dev -qq
 
 pip install frappe-bench
 
-git clone https://github.com/frappe/frappe --branch version-14-beta --depth 1
+git clone https://github.com/frappe/frappe --branch develop --depth 1
 bench init --skip-assets --frappe-path ~/frappe --python "$(which python)" frappe-bench
 
 mkdir ~/frappe-bench/sites/test_site
@@ -17,33 +17,25 @@ cp -r "${GITHUB_WORKSPACE}/.github/helper/site_config.json" ~/frappe-bench/sites
 mysql --host 127.0.0.1 --port 3306 -u root -e "SET GLOBAL character_set_server = 'utf8mb4'"
 mysql --host 127.0.0.1 --port 3306 -u root -e "SET GLOBAL collation_server = 'utf8mb4_unicode_ci'"
 
-mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE DATABASE test_frappe"
-
 mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE USER 'test_frappe'@'localhost' IDENTIFIED BY 'test_frappe'"
+mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE DATABASE test_frappe"
 mysql --host 127.0.0.1 --port 3306 -u root -e "GRANT ALL PRIVILEGES ON \`test_frappe\`.* TO 'test_frappe'@'localhost'"
-
-mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE USER 'test_frappe'@'172.18.0.1' IDENTIFIED BY 'test_frappe'"
-mysql --host 127.0.0.1 --port 3306 -u root -e "GRANT ALL PRIVILEGES ON \`test_frappe\`.* TO 'test_frappe'@'172.18.0.1'"
 
 mysql --host 127.0.0.1 --port 3306 -u root -e "UPDATE mysql.user SET Password=PASSWORD('travis') WHERE User='root'"
 mysql --host 127.0.0.1 --port 3306 -u root -e "FLUSH PRIVILEGES"
 
-
 cd ~/frappe-bench || exit
-
 
 sed -i 's/watch:/# watch:/g' Procfile
 sed -i 's/schedule:/# schedule:/g' Procfile
 sed -i 's/socketio:/# socketio:/g' Procfile
 sed -i 's/redis_socketio:/# redis_socketio:/g' Procfile
 
-bench get-app erpnext --branch version-14-beta
-bench setup requirements --dev
+bench get-app erpnext --branch develop
 
 bench start &
 bench --site test_site reinstall --yes
 
-bench --verbose --site test_site install-app erpnext
-
 bench get-app hrms "${GITHUB_WORKSPACE}"
-bench --verbose --site test_site install-app hrms
+bench --site test_site install-app hrms
+bench setup requirements --dev
