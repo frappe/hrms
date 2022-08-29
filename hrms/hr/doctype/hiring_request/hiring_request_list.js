605b4f8bede987b3ea478cef1dc8f7c2b1179214
Rucha Mahabal <ruchamahabal2@gmail.com>
2022-08-29 19:50:48 +0530

    feat: Hiring Request

---
+++ hrms/hr/doctype/hiring_request/hiring_request_list.js
@@ -0,0 +1,34 @@
+frappe.listview_settings["Hiring Request"] = {
+	get_indicator: function(doc) {
+		let status_color = {
+			"Open": "yellow",
+			"Approved": "blue",
+			"Rejected": "red",
+			"Filled": "green",
+			"Cancelled": "gray",
+			"On Hold": "gray"
+		};
+		return [__(doc.status), status_color[doc.status], "status,=,"+doc.status];
+	},
+	formatters: {
+		expected_by(value, df, doc) {
+			if (!value) return ""
+			if (["Filled", "Cancelled", "On Hold"].includes(doc.status)) return "";
+
+			let d = moment(value);
+			let now = moment();
+			let color = "green";
+			if (d < now) {
+				color = "red"
+			}
+
+			return `
+				<div
+					class="pill"
+					style="background-color: var(--bg-${color}); color: var(--text-on-${color}); font-weight:500">
+					${d.fromNow()}
+				</div>
+			`;
+		}
+	}
+}
