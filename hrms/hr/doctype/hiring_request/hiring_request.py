662b9ac753b6ed538e2aa5feaf245712d8e04786
Rucha Mahabal <ruchamahabal2@gmail.com>
2022-08-29 23:35:26 +0530

    feat: Time to Fill metric for Hiring Requests

--- hrms/hr/doctype/hiring_request/hiring_request.py
+++ hrms/hr/doctype/hiring_request/hiring_request.py
@@ -5,10 +5,14 @@ import frappe
 from frappe import _
 from frappe.model.document import Document
 from frappe.model.mapper import get_mapped_doc
-from frappe.utils import get_link_to_form
+from frappe.utils import format_duration, get_link_to_form, time_diff_in_seconds
 
 
 class HiringRequest(Document):
+	def validate(self):
+		if self.status == "Filled" and self.completed_on:
+			self.time_to_fill = time_diff_in_seconds(self.completed_on, self.posting_date)
+
 	@frappe.whitelist()
 	def associate_job_opening(self, job_opening):
 		frappe.db.set_value(
@@ -46,3 +50,22 @@ def make_job_opening(source_name, target_doc=None):
 		target_doc,
 		set_missing_values,
 	)
+
+
+@frappe.whitelist()
+def get_avg_time_to_fill(company=None, department=None, designation=None):
+	filters = {"status": "Filled"}
+	if company:
+		filters["company"] = company
+	if department:
+		filters["department"] = department
+	if designation:
+		filters["designation"] = designation
+
+	avg_time_to_fill = frappe.db.get_all(
+		"Hiring Request",
+		filters=filters,
+		fields=["avg(time_to_fill) as average_time"],
+	)[0].average_time
+
+	return format_duration(avg_time_to_fill) if avg_time_to_fill else 0
--- hrms/hr/doctype/job_opening/job_opening.py
+++ hrms/hr/doctype/job_opening/job_opening.py
@@ -6,7 +6,7 @@
 
 import frappe
 from frappe import _
-from frappe.utils import get_link_to_form
+from frappe.utils import get_link_to_form, getdate
 from frappe.website.website_generator import WebsiteGenerator
 
 from hrms.hr.doctype.staffing_plan.staffing_plan import (
@@ -26,6 +26,7 @@ class JobOpening(WebsiteGenerator):
 		if not self.route:
 			self.route = frappe.scrub(self.job_title).replace("_", "-")
 		self.validate_current_vacancies()
+		self.update_hiring_request_status()
 
 	def validate_current_vacancies(self):
 		if not self.staffing_plan:
@@ -61,6 +62,15 @@ class JobOpening(WebsiteGenerator):
 					),
 					title=_("Vacancies fulfilled"),
 				)
+
+	def update_hiring_request_status(self):
+		if self.status == "Closed" and self.hiring_request:
+			hiring_request = frappe.get_doc("Hiring Request", self.hiring_request)
+			hiring_request.status = "Filled"
+			hiring_request.completed_on = getdate()
+			hiring_request.flags.ignore_permissions = True
+			hiring_request.flags.ignore_mandatory = True
+			hiring_request.save()
 
 	def get_context(self, context):
 		context.parents = [{"route": "jobs", "title": _("All Jobs")}]
