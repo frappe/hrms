02bb13dacea83835d2884ae5b5175274c1303c11
Rucha Mahabal <ruchamahabal2@gmail.com>
2022-08-30 10:19:19 +0530

    feat: Appraisal
    - Details tab: basic details about appraisal cycle and employee
    
    - KRAs: table for KRA, weightage, goal completion status against the KRA and score
    
    - Feedbacks tab: add edit delete feedbacks (360 degree)
    
    (cherry picked from commit 291afb5ef2750ecda5707435856d27f99c49b7cf)

--- hrms/hr/doctype/appraisal/appraisal.js
+++ hrms/hr/doctype/appraisal/appraisal.js
@@ -1,79 +1,221 @@
 // Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 // License: GNU General Public License v3. See license.txt
+frappe.provide("hrms");
 
-frappe.ui.form.on('Appraisal', {
-	setup: function(frm) {
-		frm.add_fetch('employee', 'company', 'company');
-		frm.add_fetch('employee', 'employee_name', 'employee_name');
-		frm.fields_dict.employee.get_query = function(doc,cdt,cdn) {
-			return{	query: "erpnext.controllers.queries.employee_query" }
-		};
-	},
+frappe.ui.form.on("Appraisal", {
+	refresh: function(frm) {
+		if (!frm.doc._islocal) {
+			frm.add_custom_button(__("View Goals"), function() {
+				frappe.route_options = {
+					company: frm.doc.company,
+					employee: frm.doc.employee,
+				};
+				frappe.set_route("Tree", "Goal");
+			});
 
-	onload: function(frm) {
-		if(!frm.doc.status) {
-			frm.set_value('status', 'Draft');
+			frm.trigger("show_feedbacks");
+			frm.trigger("setup_chart");
 		}
 	},
 
 	kra_template: function(frm) {
-		frm.doc.goals = [];
-		erpnext.utils.map_current_doc({
-			method: "hrms.hr.doctype.appraisal.appraisal.fetch_appraisal_template",
-			source_name: frm.doc.kra_template,
-			frm: frm
+		if (frm.doc.kra_template) {
+			frappe.call({
+				"method": "frappe.client.get",
+				args: {
+					doctype: "Appraisal Template",
+					name: frm.doc.kra_template
+				},
+				callback: function(data) {
+					frm.doc.appraisal_kra = [];
+					$.each(data.message.goals, function(_i, e) {
+						let entry = frm.add_child("appraisal_kra");
+						entry.kra = e.kra;
+						entry.per_weightage = e.per_weightage;
+					});
+					refresh_field("appraisal_kra");
+				}
+			});
+		}
+	},
+
+	show_feedbacks: function(frm) {
+		const feedbacks = new hrms.Feedback({
+			frm: frm,
+			feedback_wrapper: $(frm.fields_dict.feedback_html.wrapper),
 		});
+		feedbacks.refresh();
 	},
 
-	calculate_total: function(frm) {
-	  	let goals = frm.doc.goals || [];
-		let total = 0;
+	setup_chart: function(frm) {
+		let labels = [];
+		let maximum_scores = [];
+		let scores = [];
 
-		if (goals == []) {
-			frm.set_value('total_score', 0);
-			return;
-		}
-		for (let i = 0; i<goals.length; i++) {
-			total = flt(total)+flt(goals[i].score_earned)
-		}
-		if (!isNaN(total)) {
-			frm.set_value('total_score', total);
-			frm.refresh_field('calculate_total');
-		}
-	},
+		$.each(frm.doc.appraisal_kra, function(_i, e) {
+			labels.push(e.kra);
+			maximum_scores.push(e.per_weightage);
+			scores.push(e.goal_score);
+		});
 
-	set_score_earned: function(frm) {
-		let goals = frm.doc.goals || [];
-		for (let i = 0; i<goals.length; i++) {
-			var d = locals[goals[i].doctype][goals[i].name];
-			if (d.score && d.per_weightage) {
-				d.score_earned = flt(d.per_weightage*d.score, precision("score_earned", d))/100;
-			}
-			else {
-				d.score_earned = 0;
-			}
-			refresh_field('score_earned', d.name, 'goals');
+		if (labels.length && maximum_scores.length && scores.length) {
+			frm.dashboard.render_graph({
+				data: {
+					labels: labels,
+					datasets: [
+						{
+							name: "Maximum Score",
+							chartType: "bar",
+							values: maximum_scores,
+						},
+						{
+							name: "Score Obtained",
+							chartType: "bar",
+							values: scores,
+						}
+					]
+				},
+
+				title: __("Scores"),
+				height: 300,
+				colors: ["#4CA746", "#98D85B"],
+				type: "bar"
+			});
 		}
-		frm.trigger('calculate_total');
 	}
+
 });
 
-frappe.ui.form.on('Appraisal Goal', {
-	score: function(frm, cdt, cdn) {
-		var d = locals[cdt][cdn];
-		if (flt(d.score) > 5) {
-			frappe.msgprint(__("Score must be less than or equal to 5"));
-			d.score = 0;
-			refresh_field('score', d.name, 'goals');
-		}
-		else {
-			frm.trigger('set_score_earned');
-		}
-	},
-	per_weightage: function(frm) {
-		frm.trigger('set_score_earned');
-	},
-	goals_remove: function(frm) {
-		frm.trigger('set_score_earned');
+hrms.Feedback = class Feedback {
+	constructor(opts) {
+		$.extend(this, opts);
 	}
-});
+
+	refresh() {
+		var me = this;
+		this.feedback_wrapper.find(".notes-section").remove();
+
+		let feedbacks = this.frm.doc.feedbacks_table || [];
+		feedbacks.sort(
+			function(a, b) {
+				return new Date(b.added_on) - new Date(a.added_on);
+			}
+		);
+
+		let feedback_html = frappe.render_template("feedback", {
+			feedbacks: feedbacks
+		});
+		$(feedback_html).appendTo(this.feedback_wrapper);
+
+		this.add_feedback();
+
+		$(".notes-section").find(".edit-note-btn").on("click", function() {
+			me.edit_feedback(this);
+		});
+
+		$(".notes-section").find(".delete-note-btn").on("click", function() {
+			me.delete_feedback(this);
+		});
+	}
+
+
+	add_feedback () {
+		let me = this;
+		let _add_feedback = () => {
+			var d = new frappe.ui.Dialog({
+				title: __("Add Feedback"),
+				fields: [
+					{
+						"label": "Feedback",
+						"fieldname": "feedback",
+						"fieldtype": "Text Editor",
+						"reqd": 1,
+						"enable_mentions": true,
+					}
+				],
+				primary_action: function() {
+					var data = d.get_values();
+					frappe.call({
+						method: "add_feedback",
+						doc: me.frm.doc,
+						args: {
+							feedback: data.feedback
+						},
+						freeze: true,
+						callback: function(r) {
+							if (!r.exc) {
+								me.frm.refresh_field("feedbacks_table");
+								me.refresh();
+							}
+							d.hide();
+						}
+					});
+				},
+				primary_action_label: __("Add")
+			});
+			d.show();
+		};
+		$(".new-note-btn").click(_add_feedback);
+	}
+
+	edit_feedback (edit_btn) {
+		var me = this;
+		let row = $(edit_btn).closest(".comment-content");
+		let row_id = row.attr("name");
+		let row_content = $(row).find(".content").html();
+		if (row_content) {
+			var d = new frappe.ui.Dialog({
+				title: __("Edit Feedback"),
+				fields: [
+					{
+						"label": "Feedback",
+						"fieldname": "feedback",
+						"fieldtype": "Text Editor",
+						"reqd": 1,
+						"enable_mentions": true,
+					}
+				],
+				primary_action: function() {
+					var data = d.get_values();
+					frappe.call({
+						method: "edit_feedback",
+						doc: me.frm.doc,
+						args: {
+							feedback: data.feedback,
+							row_id: row_id
+						},
+						freeze: true,
+						callback: function(r) {
+							if (!r.exc) {
+								me.frm.refresh_field("feedbacks_table");
+								me.refresh();
+							}
+							d.hide();
+						}
+					});
+				},
+				primary_action_label: __("Done")
+			});
+			d.show();
+		}
+	}
+
+	delete_feedback (delete_btn) {
+		var me = this;
+		let row_id = $(delete_btn).closest(".comment-content").attr("name");
+		frappe.call({
+			method: "delete_feedback",
+			doc: me.frm.doc,
+			args: {
+				row_id: row_id
+			},
+			freeze: true,
+			callback: function(r) {
+				if (!r.exc) {
+					me.frm.refresh_field("feedbacks_table");
+					me.refresh();
+				}
+			}
+		});
+	}
+};
--- hrms/hr/doctype/appraisal/appraisal.py
+++ hrms/hr/doctype/appraisal/appraisal.py
@@ -5,10 +5,9 @@
 import frappe
 from frappe import _
 from frappe.model.document import Document
-from frappe.model.mapper import get_mapped_doc
-from frappe.utils import flt, getdate
+from frappe.utils import cstr, flt, now
 
-from hrms.hr.utils import set_employee_name, validate_active_employee
+from hrms.hr.utils import validate_active_employee
 
 
 class Appraisal(Document):
@@ -16,79 +15,85 @@ class Appraisal(Document):
 		if not self.status:
 			self.status = "Draft"
 
-		if not self.goals:
-			frappe.throw(_("Goals cannot be empty"))
-
 		validate_active_employee(self.employee)
-		set_employee_name(self)
-		self.validate_dates()
 		self.validate_existing_appraisal()
-		self.calculate_total()
-
-	def get_employee_name(self):
-		self.employee_name = frappe.db.get_value("Employee", self.employee, "employee_name")
-		return self.employee_name
-
-	def validate_dates(self):
-		if getdate(self.start_date) > getdate(self.end_date):
-			frappe.throw(_("End Date can not be less than Start Date"))
+		self.calculate_total_score()
+		self.feedbacks = len(self.feedbacks_table)
 
 	def validate_existing_appraisal(self):
-		chk = frappe.db.sql(
+		duplicate = frappe.db.sql(
 			"""select name from `tabAppraisal` where employee=%s
 			and (status='Submitted' or status='Completed')
-			and ((start_date>=%s and start_date<=%s)
-			or (end_date>=%s and end_date<=%s))""",
-			(self.employee, self.start_date, self.end_date, self.start_date, self.end_date),
+			and (appraisal_cycle=%s)""",
+			(self.employee, self.appraisal_cycle),
 		)
-		if chk:
+		if duplicate:
 			frappe.throw(
-				_("Appraisal {0} created for Employee {1} in the given date range").format(
-					chk[0][0], self.employee_name
+				_("Appraisal {0} already created for Employee {1} for this Appraisal Cycle").format(
+					duplicate[0][0], self.employee_name
 				)
 			)
 
-	def calculate_total(self):
-		total, total_w = 0, 0
-		for d in self.get("goals"):
-			if d.score:
-				d.score_earned = flt(d.score) * flt(d.per_weightage) / 100
-				total = total + d.score_earned
-			total_w += flt(d.per_weightage)
-
-		if int(total_w) != 100:
-			frappe.throw(
-				_("Total weightage assigned should be 100%.<br>It is {0}").format(str(total_w) + "%")
-			)
-
-		if (
-			frappe.db.get_value("Employee", self.employee, "user_id") != frappe.session.user and total == 0
-		):
-			frappe.throw(_("Total cannot be zero"))
+	def calculate_total_score(self):
+		total = 0
+		for entry in self.appraisal_kra:
+			total += entry.goal_score
 
 		self.total_score = total
 
-	def on_submit(self):
-		frappe.db.set(self, "status", "Submitted")
-
-	def on_cancel(self):
-		frappe.db.set(self, "status", "Cancelled")
-
-
-@frappe.whitelist()
-def fetch_appraisal_template(source_name, target_doc=None):
-	target_doc = get_mapped_doc(
-		"Appraisal Template",
-		source_name,
-		{
-			"Appraisal Template": {
-				"doctype": "Appraisal",
+	@frappe.whitelist()
+	def add_feedback(self, feedback):
+		self.append(
+			"feedbacks_table",
+			{
+				"feedback": feedback,
+				"given_by": frappe.db.get_value("Employee", {"user_id": frappe.session.user}),
+				"user": frappe.session.user,
+				"added_on": now(),
+				"for_employee": self.employee,
 			},
-			"Appraisal Template Goal": {
-				"doctype": "Appraisal Goal",
-			},
-		},
-		target_doc,
+		)
+		self.save()
+
+	@frappe.whitelist()
+	def edit_feedback(self, feedback, row_id):
+		for d in self.feedbacks_table:
+			if cstr(d.name) == row_id:
+				d.feedback = feedback
+				d.db_update()
+
+	@frappe.whitelist()
+	def delete_feedback(self, row_id):
+		for d in self.feedbacks_table:
+			if cstr(d.name) == row_id:
+				self.remove(d)
+				break
+		self.save()
+
+
+def update_progress_in_appraisal(goal):
+	appraisal = frappe.db.exists(
+		"Appraisal", {"employee": goal.employee, "appraisal_cycle": goal.appraisal_cycle}
 	)
+	if not appraisal:
+		return
 
-	return target_doc
+	doc = frappe.get_doc("Appraisal", appraisal)
+	for kra in doc.appraisal_kra:
+		total_goals = frappe.db.count(
+			"Goal", {"kra": kra.kra, "employee": doc.employee, "status": ("!=", "Archived")}
+		)
+		completed_goals = frappe.db.count(
+			"Goal",
+			{
+				"kra": kra.kra,
+				"status": "Completed",
+				"employee": doc.employee,
+			},
+		)
+		kra.goal_completion = (
+			flt(completed_goals / total_goals * 100, kra.precision("goal_completion")) if total_goals else 0
+		)
+		kra.goal_score = flt(kra.goal_completion * kra.per_weightage / 100, kra.precision("goal_score"))
+
+	doc.save()
