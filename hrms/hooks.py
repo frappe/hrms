b10b968aecf6e36798e8796934e1e0c056178b54
batonac <kevin@avu.nu>
2022-09-28 12:12:53 -0400

    include the js bundle

--- hrms/hooks.py
+++ hrms/hooks.py
@@ -12,7 +12,7 @@ required_apps = ["erpnext"]
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/hrms/css/hrms.css"
-# app_include_js = "/assets/hrms/js/hrms.js"
+app_include_js = "hrms.bundle.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/hrms/css/hrms.css"
