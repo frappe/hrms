211eb8cc1590bce166b01815d71c7a86a9b10844
Ankush Menat <ankush@frappe.io>
2022-10-04 17:45:14 +0530

    refactor: remove client side code for current datetime (#80)

--- hrms/hr/doctype/employee_checkin/employee_checkin.js
+++ hrms/hr/doctype/employee_checkin/employee_checkin.js
@@ -2,9 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Employee Checkin', {
-	setup: (frm) => {
-		if(!frm.doc.time) {
-			frm.set_value("time", frappe.datetime.now_datetime());
-		}
-	}
+	// setup: (frm) => {
+
+	// }
 });
