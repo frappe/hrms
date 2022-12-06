a299673831a33c3de3ab975369eda5043505794e
Dany Robert <danyrt@wahni.com>
2022-12-06 12:18:43 +0000

    fix: pass ll entries for overlapping allocation

--- hrms/hr/doctype/auto_leave_allocation/auto_leave_allocation.py
+++ hrms/hr/doctype/auto_leave_allocation/auto_leave_allocation.py
@@ -6,7 +6,9 @@ import json
 import frappe
 from frappe import _
 from frappe.model.document import Document
-from frappe.utils import add_to_date, get_last_day, get_last_day_of_week, getdate
+from frappe.utils import add_to_date, flt, formatdate, get_last_day, get_last_day_of_week, getdate
+
+from hrms.hr.doctype.leave_ledger_entry.leave_ledger_entry import create_leave_ledger_entry
 
 
 class OverlapError(frappe.ValidationError):
@@ -34,6 +36,7 @@ class AutoLeaveAllocation(Document):
 			.select(db_ala.name)
 			.where(db_ala.docstatus != 2)
 			.where(db_ala.leave_type == self.leave_type)
+			.where(db_ala.filters_json == self.filters_json)
 			.where(
 				((db_ala.start_date >= self.start_date) & (db_ala.start_date <= self.end_date))
 				| ((db_ala.end_date >= self.start_date) & (db_ala.end_date <= self.end_date))
@@ -56,11 +59,19 @@ class AutoLeaveAllocation(Document):
 		to_date = (
 			get_last_day(from_date) if self.frequency == "Monthly" else get_last_day_of_week(from_date)
 		)
+		to_date = min(to_date, getdate(self.end_date))
 		conditions = json.loads(self.filters_json) or []
 		conditions.append(["Employee", "status", "=", "Active"])
 		employees = frappe.db.get_all("Employee", filters=conditions, pluck="name")
 		for emp in employees:
 			try:
+				# get_overlapping_entries only considers super-sets of the leave allocation it plans to create.
+				overlapping_entries = get_overlapping_entries(emp, self.leave_type, from_date, to_date)
+				if overlapping_entries:
+					update_overlapping_leave_allocation(
+						overlapping_entries[0][0], self.leave_type, self.new_allocation, from_date, to_date
+					)
+					continue
 				frappe.get_doc(
 					{
 						"doctype": "Leave Allocation",
@@ -73,8 +84,10 @@ class AutoLeaveAllocation(Document):
 						"auto_leave_allocation": self.name,
 					}
 				).submit()
-			except Exception as e:
-				frappe.log_error(title=f"Could not auto allocate leave for {emp}", message=str(e))
+			except Exception:
+				frappe.log_error(
+					title=f"Could not auto allocate leave for {emp}", message=str(frappe.get_traceback())
+				)
 				continue
 
 	@frappe.whitelist()
@@ -118,3 +131,47 @@ def process_weekly_auto_leave_allocation():
 
 def process_monthly_auto_leave_allocation():
 	process_auto_leave_allocation("Monthly")
+
+
+def update_overlapping_leave_allocation(allocation, leave_type, earned_leaves, from_date, to_date):
+	leave_type = frappe.get_cached_doc("Leave Type", leave_type)
+	allocation = frappe.get_doc("Leave Allocation", allocation)
+	new_allocation = flt(allocation.total_leaves_allocated) + flt(earned_leaves)
+
+	if new_allocation > leave_type.max_leaves_allowed and leave_type.max_leaves_allowed > 0:
+		new_allocation = leave_type.max_leaves_allowed
+
+	if new_allocation != allocation.total_leaves_allocated:
+		allocation.db_set("total_leaves_allocated", new_allocation, update_modified=False)
+		args = dict(
+			leaves=earned_leaves,
+			from_date=from_date,
+			to_date=to_date,
+			is_carry_forward=0,
+		)
+		create_leave_ledger_entry(allocation, args)
+
+		if leave_type.based_on_date_of_joining:
+			text = _("allocated {0} leave(s) via scheduler on {1} based on the date of joining").format(
+				frappe.bold(earned_leaves), frappe.bold(formatdate(from_date))
+			)
+		else:
+			text = _("allocated {0} leave(s) via scheduler on {1}").format(
+				frappe.bold(earned_leaves), frappe.bold(formatdate(from_date))
+			)
+
+		allocation.add_comment(comment_type="Info", text=text)
+
+
+def get_overlapping_entries(employee, leave_type, from_date, to_date):
+	Allocation = frappe.qb.DocType("Leave Allocation")
+	return (
+		frappe.qb.from_(Allocation)
+		.select(Allocation.name)
+		.where(
+			(Allocation.employee == employee)
+			& (Allocation.leave_type == leave_type)
+			& (Allocation.docstatus == 1)
+			& ((Allocation.from_date <= from_date) & (Allocation.to_date >= to_date))
+		)
+	).run()
