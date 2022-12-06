5dfeb62ce673ed01c25c0ecfca29256083b0e862
Dany Robert <danyrt@wahni.com>
2022-12-06 12:19:01 +0000

    fix: confirmation before manual allocation

--- hrms/hr/doctype/auto_leave_allocation/auto_leave_allocation.js
+++ hrms/hr/doctype/auto_leave_allocation/auto_leave_allocation.js
@@ -9,14 +9,20 @@ frappe.ui.form.on('Auto Leave Allocation', {
 		frm.trigger("render_filters_table");
 		if (frm.doc.docstatus === 1 && frm.doc.start_date <= frappe.datetime.get_today()) {
 			frm.add_custom_button(__("Allocate"), function () {
-				frm.call('run_allocation').then(r => {
-					if (r.message) {
-						frappe.show_alert({
-							message: __('Executed allocation. Check error log for any issue(s).'),
-							indicator: 'green'
-						}, 5);
-					}
-				});
+				frappe.confirm(__('Are you sure you want to proceed? This might cause duplicate entries.'),
+					() => {
+						// action to perform if Yes is selected
+						frm.call('run_allocation').then(r => {
+							if (r.message) {
+								frappe.show_alert({
+									message: __('Executed allocation. Check error log for any issue(s).'),
+									indicator: 'green'
+								}, 5);
+							}
+						});
+					}, () => {
+						// action to perform if No is selected
+					})
 			});
 		}
 	},
