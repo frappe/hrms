91a23488557bc71e289d4f35486dc9bfe3a182d4
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-01-03 12:28:16 +0530

    fix(tests): rectify logic for getting the first day of prev month

--- hrms/tests/test_utils.py
+++ hrms/tests/test_utils.py
@@ -1,5 +1,5 @@
 import frappe
-from frappe.utils import get_first_day, get_last_day, getdate
+from frappe.utils import add_months, get_first_day, get_last_day, getdate
 
 
 def get_first_sunday(holiday_list="Salary Slip Test Holiday List", for_date=None):
@@ -17,3 +17,9 @@ def get_first_sunday(holiday_list="Salary Slip Test Holiday List", for_date=None
 	)[0][0]
 
 	return first_sunday
+
+
+def get_first_day_for_prev_month():
+	prev_month = add_months(getdate(), -1)
+	prev_month_first = prev_month.replace(day=1)
+	return prev_month_first
