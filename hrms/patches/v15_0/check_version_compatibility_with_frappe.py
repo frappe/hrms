d74f09a89a07eacaea3d1059ccbdb7d86b33ead3
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-02-02 17:53:05 +0530

    fix: Allow skipping patch

--- hrms/patches/v15_0/check_version_compatibility_with_frappe.py
+++ hrms/patches/v15_0/check_version_compatibility_with_frappe.py
@@ -10,14 +10,11 @@ def execute():
 	WIKI_URL = "https://github.com/frappe/hrms/wiki/Changes-to-branching-and-versioning"
 
 	if "14" in frappe_v and "15" in hrms_v:
-		click.secho(
-			f"""
+		message = f"""
 			The `develop` branch of Frappe HR is no longer compatible with Frappe & ERPNext's `version-14`.
 			Since you are using ERPNext/Frappe `version-14` please switch Frappe HR's branch to `version-14` and then proceed with the update.\n\t
 			You can switch the branch by following the steps mentioned here: {WIKI_URL}
-			""",
-			fg="red",
-		)
+		"""
+		click.secho(message, fg="red")
 
-		# don't skip this patch to enforce switching branches
-		raise SystemExit(1)
+		frappe.throw(message)  # nosemgrep
