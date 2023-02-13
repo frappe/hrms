652503c018f487dd0ec7d6e8b743fe3d43032c73
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-02-13 20:03:46 +0530

    chore: fix linter

--- hrms/hr/doctype/job_applicant/job_applicant.py
+++ hrms/hr/doctype/job_applicant/job_applicant.py
@@ -45,7 +45,9 @@ class JobApplicant(Document):
 		if self.job_title:
 			job_opening_status = frappe.db.get_value("Job Opening", self.job_title, "status")
 			if job_opening_status == "Closed":
-				frappe.throw(_("Cannot create a Job Applicant against a closed Job Opening"), title=_("Not Allowed"))
+				frappe.throw(
+					_("Cannot create a Job Applicant against a closed Job Opening"), title=_("Not Allowed")
+				)
 
 	def set_status_for_employee_referral(self):
 		emp_ref = frappe.get_doc("Employee Referral", self.employee_referral)
