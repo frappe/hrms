2ba58de63b255338995d5ad1eb4e350a1ced0437
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-02-23 23:30:13 +0530

    refactor: Performance Feedback
    - set KRAs from template on server side
    
    - change naming from hash to naming series
    
    - validate duplicate appraisals before insert
    
    - remove redundant field for feedback count
    
    (cherry picked from commit fb05d3e58a5bae1c572ce613b79255facdce6c3c)

--- hrms/hr/doctype/performance_feedback/performance_feedback.js
+++ hrms/hr/doctype/performance_feedback/performance_feedback.js
@@ -1,26 +1,21 @@
 // Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Performance Feedback', {
-	// refresh: function(frm) {
-
-	// },
-	employee: function(frm) {
-		if (frm.doc.employee) {
-			frappe.call({
-				"method": "hrms.hr.doctype.performance_feedback.performance_feedback.get_kra",
-				args: {
-					employee: frm.doc.employee
-				},
-				callback: function(data) {
-					frm.doc.kra_rating = [];
-					$.each(data.message.goals, function(_i, e) {
-						const entry = frm.add_child("kra_rating");
-						entry.kra = e.kra;
-						entry.per_weightage = e.per_weightage;
-					});
-					refresh_field("kra_rating");
+frappe.ui.form.on("Performance Feedback", {
+	refresh: function(frm) {
+		frm.set_query("appraisal", () => {
+			return {
+				filters: {
+					employee: frm.doc.employee,
 				}
+			}
+		});
+	},
+
+	appraisal: function(frm) {
+		if (frm.doc.employee) {
+			frm.call("set_kras", () => {
+				frm.refresh_field("kra_rating");
 			});
 		}
 	},
