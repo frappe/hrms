e45e7958f17931adaf2f6a3290ca29e50ac52d89
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-03-17 19:36:06 +0530

    test: Appraisal Template

--- hrms/hr/doctype/appraisal_template/test_appraisal_template.py
+++ hrms/hr/doctype/appraisal_template/test_appraisal_template.py
@@ -3,8 +3,43 @@
 
 import unittest
 
-# test_records = frappe.get_test_records('Appraisal Template')
+import frappe
+from frappe.tests.utils import FrappeTestCase
 
 
-class TestAppraisalTemplate(unittest.TestCase):
-	pass
+class TestAppraisalTemplate(FrappeTestCase):
+	def test_incorrect_weightage_allocation(self):
+		create_kras()
+
+		appraisal_template = frappe.new_doc("Appraisal Template")
+		appraisal_template.template_title = "Engineering"
+		appraisal_template.update(
+			{
+				"goals": [
+					{
+						"kra": "Quality",
+						"per_weightage": 30,
+					},
+					{
+						"kra": "Development",
+						"per_weightage": 69.99,
+					},
+				]
+			}
+		)
+
+		self.assertRaises(frappe.ValidationError, appraisal_template.insert)
+
+		appraisal_template.goals[1].per_weightage = 70.00
+		appraisal_template.insert()
+
+
+def create_kras():
+	for entry in ["Quality", "Development"]:
+		if not frappe.db.exists("KRA", entry):
+			kra = frappe.get_doc(
+				{
+					"doctype": "KRA",
+					"title": entry,
+				}
+			).insert()
