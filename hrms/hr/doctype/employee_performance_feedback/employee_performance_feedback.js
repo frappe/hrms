6ce3b3659016a134e3f5ff15f3a29daf87690d70
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-03-21 12:57:29 +0530

    refactor(UX): Employee Perf Feedback
    - set filters for reviewer and employee
    
    - set current session employee as reviewer
    
    - set search, list view and filter fields
    
    (cherry picked from commit 0c4b04902e00d57397242d04be90ad424d24b651)

--- hrms/hr/doctype/employee_performance_feedback/employee_performance_feedback.js
+++ hrms/hr/doctype/employee_performance_feedback/employee_performance_feedback.js
@@ -2,7 +2,23 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Employee Performance Feedback", {
-	refresh: function(frm) {
+	onload(frm) {
+		frm.trigger("set_reviewer");
+	},
+
+	refresh(frm) {
+		frm.trigger("set_filters");
+	},
+
+	appraisal(frm) {
+		if (frm.doc.employee) {
+			frm.call("set_feedback_criteria", () => {
+				frm.refresh_field("feedback_ratings");
+			});
+		}
+	},
+
+	set_filters(frm) {
 		frm.set_query("appraisal", () => {
 			return {
 				filters: {
@@ -11,13 +27,31 @@ frappe.ui.form.on("Employee Performance Feedback", {
 				}
 			}
 		});
+
+		frm.set_query("reviewer", () => {
+			return {
+				filters: {
+					employee: ["!=", frm.doc.employee],
+				}
+			}
+		});
+
+		frm.set_query("employee", () => {
+			return {
+				filters: {
+					employee: ["!=", frm.doc.reviewer],
+				}
+			}
+		});
 	},
 
-	appraisal: function(frm) {
-		if (frm.doc.employee) {
-			frm.call("set_feedback_criteria", () => {
-				frm.refresh_field("feedback_ratings");
+	set_reviewer(frm) {
+		if (!frm.doc.reviewer) {
+			frappe.db.get_value("Employee", { user_id: frappe.session.user }, "name").then(employee_record => {
+				const session_employee = employee_record?.message?.name;
+				if (session_employee)
+					frm.set_value("reviewer", session_employee);
 			});
 		}
-	},
+	}
 });
