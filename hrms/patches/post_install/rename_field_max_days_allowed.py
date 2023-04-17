7612a8eeab05fb05399ef922c2ce4b735b561764
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-04-17 22:37:59 +0530

    refactor: fail-safe post install patches
    - add ignore mandatory flags wherever applicable
    
    - clear dt cache after altering table directly
    
    - convert subquery updates to join query updates in QB
    
    - make sure data doesn't get overwritten if patch runs more than once
    
    - remove unnecessary post install patches

--- hrms/patches/post_install/rename_field_max_days_allowed.py
+++ hrms/patches/post_install/rename_field_max_days_allowed.py
@@ -16,3 +16,8 @@ def execute():
 	frappe.db.sql_ddl("""ALTER table `tabLeave Type` modify max_days_allowed int(8) NOT NULL""")
 	frappe.reload_doc("hr", "doctype", "leave_type")
 	rename_field("Leave Type", "max_days_allowed", "max_continuous_days_allowed")
+
+	if frappe.db.has_column("Leave Type", "max_days_allowed"):
+		frappe.db.sql("alter table `tabLeave Type` drop column max_days_allowed")
+		# clear cache for doctype as it stores table columns in cache
+		frappe.clear_cache(doctype="Leave Type")
