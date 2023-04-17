e549fe95519338356cbf4bca9e6cdccefd7c4b66
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-04-17 22:37:59 +0530

    refactor: fail-safe post install patches
    - add ignore mandatory flags wherever applicable
    
    - clear dt cache after altering table directly
    
    - convert subquery updates to join query updates in QB
    
    - make sure data doesn't get overwritten if patch runs more than once
    
    - remove unnecessary post install patches
    
    (cherry picked from commit 7612a8eeab05fb05399ef922c2ce4b735b561764)

--- hrms/patches/post_install/drop_column_max_days_allowed.py
+++
@@ -1,7 +0,0 @@
-import frappe
-
-
-def execute():
-	if frappe.db.exists("DocType", "Leave Type"):
-		if frappe.db.has_column("Leave Type", "max_days_allowed"):
-			frappe.db.sql("alter table `tabLeave Type` drop column max_days_allowed")
