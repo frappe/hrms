b3a3b1ad4c2c800cd0829487c30d49739b14852a
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-05-02 14:25:51 +0530

    fix: consider grade filter

--- hrms/hr/doctype/leave_control_panel/leave_control_panel.py
+++ hrms/hr/doctype/leave_control_panel/leave_control_panel.py
@@ -11,10 +11,20 @@ from frappe.utils import cint, comma_and, cstr, flt
 class LeaveControlPanel(Document):
 	def get_employees(self):
 		conditions, values = [], []
-		for field in ["company", "employment_type", "branch", "designation", "department", "employee"]:
+		for field in [
+			"company",
+			"employment_type",
+			"branch",
+			"designation",
+			"department",
+			"employee",
+			"employee_grade",
+		]:
 			if self.get(field):
 				if field == "employee":
 					conditions.append("name=%s")
+				elif field == "employee_grade":
+					conditions.append("grade=%s")
 				else:
 					conditions.append("{0}=%s".format(field))
 
