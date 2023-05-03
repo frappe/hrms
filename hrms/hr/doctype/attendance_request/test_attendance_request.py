1166f794a3b8ecd883609eea622a29584fd2b63b
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-05-03 14:46:43 +0530

    test: Attendance Request overlap & skip attendance on leave
    (cherry picked from commit d975160c92d65cb1aea4cf912558e89d5684dac8)

--- hrms/hr/doctype/attendance_request/test_attendance_request.py
+++ hrms/hr/doctype/attendance_request/test_attendance_request.py
@@ -8,7 +8,12 @@ from frappe.tests.utils import FrappeTestCase
 from frappe.utils import add_days, add_months, get_year_ending, get_year_start, getdate
 
 from hrms.hr.doctype.attendance.attendance import mark_attendance
-from hrms.payroll.doctype.salary_slip.test_salary_slip import make_holiday_list
+from hrms.hr.doctype.attendance_request.attendance_request import OverlappingAttendanceRequestError
+from hrms.hr.doctype.leave_application.test_leave_application import make_allocation_record
+from hrms.payroll.doctype.salary_slip.test_salary_slip import (
+	make_holiday_list,
+	make_leave_application,
+)
 from hrms.tests.test_utils import get_first_sunday
 
 test_dependencies = ["Employee"]
@@ -19,14 +24,43 @@ class TestAttendanceRequest(FrappeTestCase):
 		for doctype in ["Attendance Request", "Attendance"]:
 			frappe.db.delete(doctype)
 
-		from_date = get_year_start(add_months(getdate(), -1))
-		to_date = get_year_ending(getdate())
+		self.from_date = get_year_start(add_months(getdate(), -1))
+		self.to_date = get_year_ending(getdate())
 		self.holiday_list = make_holiday_list(
-			from_date=from_date, to_date=to_date, add_weekly_offs=False
+			from_date=self.from_date, to_date=self.to_date, add_weekly_offs=False
 		)
 
 		self.employee = get_employee()
 		frappe.db.set_value("Employee", self.employee.name, "holiday_list", self.holiday_list)
+
+	def test_attendance_request_overlap(self):
+		create_attendance_request(employee=self.employee.name, reason="On Duty", company="_Test Company")
+
+		today = getdate()
+		dateranges = [
+			(add_days(today, -2), today),
+			(today, today),
+			(today, add_days(today, 1)),
+			(add_days(today, -2), add_days(today, 2)),
+		]
+		attendance_request = frappe.get_doc(
+			{
+				"doctype": "Attendance Request",
+				"employee": self.employee.name,
+				"reason": "On Duty",
+				"company": "_Test Company",
+			}
+		)
+
+		for entry in dateranges:
+			attendance_request.from_date = entry[0]
+			attendance_request.to_date = entry[1]
+			self.assertRaises(OverlappingAttendanceRequestError, attendance_request.save)
+
+		# no overlap
+		attendance_request.from_date = add_days(today, -3)
+		attendance_request.to_date = add_days(today, -2)
+		attendance_request.save()
 
 	def test_on_duty_attendance_request(self):
 		"Test creation of Attendance from Attendance Request, on duty."
@@ -95,13 +129,36 @@ class TestAttendanceRequest(FrappeTestCase):
 		self.assertEqual(len(records), 1)
 		self.assertEqual(records[0].status, "Present")
 
+	def test_skip_attendance_on_leave(self):
+		frappe.delete_doc_if_exists("Leave Type", "Test Skip Attendance", force=1)
+		leave_type = frappe.get_doc(
+			dict(leave_type_name="Test Skip Attendance", doctype="Leave Type")
+		).insert()
+
+		make_allocation_record(
+			leave_type=leave_type.name, from_date=self.from_date, to_date=self.to_date
+		)
+		today = getdate()
+		make_leave_application(self.employee.name, today, add_days(today, 1), leave_type.name)
+
+		attendance_request = create_attendance_request(
+			employee=self.employee.name, reason="On Duty", company="_Test Company"
+		)
+		records = self.get_attendance_records(attendance_request.name)
+
+		# only 1 attendance marked for yesterday
+		# attendance skipped for today since its a leave
+		self.assertEqual(len(records), 1)
+		self.assertEqual(records[0].attendance_date, add_days(today, -1))
+		self.assertEqual(records[0].status, "Present")
+
 	def get_attendance_records(self, attendance_request: str) -> list[dict]:
 		return frappe.db.get_all(
 			"Attendance",
 			{
 				"attendance_request": attendance_request,
 			},
-			["status", "docstatus"],
+			["status", "docstatus", "attendance_date"],
 		)
 
 
