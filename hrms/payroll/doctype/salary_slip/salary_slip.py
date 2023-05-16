a5cca22898f8b20f78c08713f029003c9902bc32
Saurabh <saurabh6790@gmail.com>
2023-05-16 14:46:34 +0530

    fix: update statistical component value in default data
    (cherry picked from commit dcc80abc9a78a7a627038c1c6e389a3741f6eaa0)

--- hrms/payroll/doctype/salary_slip/salary_slip.py
+++ hrms/payroll/doctype/salary_slip/salary_slip.py
@@ -957,6 +957,9 @@ class SalarySlip(TransactionBase):
 			if struct_row.statistical_component:
 				# update statitical component amount in reference data based on payment days
 				# since row for statistical component is not added to salary slip
+
+				self.default_data[struct_row.abbr] = amount
+
 				if struct_row.depends_on_payment_days:
 					joining_date, relieving_date = self.get_joining_and_relieving_dates()
 					payment_days_amount = (
@@ -964,8 +967,6 @@ class SalarySlip(TransactionBase):
 						if self.total_working_days
 						else 0
 					)
-
-					self.default_data[struct_row.abbr] = amount
 					self.data[struct_row.abbr] = flt(payment_days_amount, struct_row.precision("amount"))
 
 			else:
