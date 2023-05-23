495c9398189540e6460d324b94a0292d8b494648
Saurabh <saurabh6790@gmail.com>
2023-05-23 12:18:05 +0530

    fix: change fieldtype to remove currency field from total row in export
    (cherry picked from commit 8566ff963ed17ad2b5051113a91bd72397a60fde)

--- hrms/payroll/report/salary_register/salary_register.py
+++ hrms/payroll/report/salary_register/salary_register.py
@@ -183,13 +183,6 @@ def get_columns(earning_types, ded_types):
 			"fieldtype": "Float",
 			"width": 120,
 		},
-		{
-			"label": _("Currency"),
-			"fieldname": "currency",
-			"fieldtype": "Link",
-			"options": "Currency",
-			"hidden": 1,
-		},
 	]
 
 	for earning in earning_types:
@@ -247,9 +240,15 @@ def get_columns(earning_types, ded_types):
 				"options": "currency",
 				"width": 120,
 			},
+			{
+				"label": _("Currency"),
+				"fieldtype": "Data",
+				"fieldname": "currency",
+				"options": "Currency",
+				"hidden": 1,
+			},
 		]
 	)
-
 	return columns
 
 
