f50c3110ac640fc1beb68d4b39c5063601f444c6
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-06-06 10:12:38 +0530

    style: add bottom margin for leave layout

--- frontend/src/views/Leaves.vue
+++ frontend/src/views/Leaves.vue
@@ -1,8 +1,9 @@
 <template>
 	<BaseLayout pageTitle="Leaves &amp; Holidays">
 		<template #body>
-			<div class="flex flex-col items-center mt-5">
+			<div class="flex flex-col items-center mt-5 mb-7">
 				<LeaveBalance />
+
 				<div class="flex flex-col gap-7 px-4 mt-4 w-full">
 					<Button appearance="primary" class="py-2 w-full">Request a Leave</Button>
 					<div>
