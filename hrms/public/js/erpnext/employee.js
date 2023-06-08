b26d5354a86da1195bbccbee5c2297bcbee79ba9
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-06-08 14:44:15 +0530

    fix: custom js for erpnext doctypes

--- hrms/public/js/erpnext/employee.js
+++ hrms/public/js/erpnext/employee.js
@@ -2,15 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Employee", {
-	setup: function (frm) {
-		frm.set_query("leave_policy", function() {
-			return {
-				filters: {
-					"docstatus": 1
-				}
-			};
-		});
-
+	refresh: function (frm) {
 		frm.set_query("payroll_cost_center", function() {
 			return {
 				filters: {
