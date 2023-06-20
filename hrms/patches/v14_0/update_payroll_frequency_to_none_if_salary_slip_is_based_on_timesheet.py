53f38cc640afef5c8d31bcd24988bb60a6c6750e
Saurabh <saurabh6790@gmail.com>
2023-06-20 15:22:19 +0530

    fix: patch
    (cherry picked from commit ab27244c2fc7927607a7b92a06e95ff51716bf0e)

--- hrms/patches/v14_0/update_payroll_frequency_to_none_if_salary_slip_is_based_on_timesheet.py
+++ hrms/patches/v14_0/update_payroll_frequency_to_none_if_salary_slip_is_based_on_timesheet.py
@@ -4,10 +4,7 @@ import frappe
 def execute():
 	frappe.reload_doc("payroll", "doctype", "salary_structure")
 
-	salary_strucutres = frappe.get_all(
-		"Salary Structure", filters={"salary_slip_based_on_timesheet": 1}, fields=["name"]
-	)
-
-	if len(salary_strucutres) > 0:
-		for salary_strucutre in salary_strucutres:
-			frappe.db.set_value("Salary Structure", salary_strucutre.name, "payroll_frequency", "")
+	salary_structure = frappe.qb.DocType("Salary Structure")
+	frappe.qb.update(salary_structure).set(salary_structure.payroll_frequency, "").where(
+		salary_structure.salary_slip_based_on_timesheet == 1
+	).run()
