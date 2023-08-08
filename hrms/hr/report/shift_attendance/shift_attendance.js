318c59b6654c91bcbd0a4a19b16782acca7d33c6
krantheman <akash.akasht.tom@gmail.com>
2023-08-08 11:57:29 +0530

    chore(Shift Attendance): convert spaces into tabs

--- hrms/hr/report/shift_attendance/shift_attendance.js
+++ hrms/hr/report/shift_attendance/shift_attendance.js
@@ -2,50 +2,50 @@
 // For license information, please see license.txt
 
 frappe.query_reports["Shift Attendance"] = {
-  filters: [
-    {
-      fieldname: "from_date",
-      label: __("From Date"),
-      fieldtype: "Date",
-    },
-    {
-      fieldname: "to_date",
-      label: __("To Date"),
-      fieldtype: "Date",
-    },
-    {
-      fieldname: "employee",
-      label: __("Employee"),
-      fieldtype: "Link",
-      options: "Employee",
-    },
-    {
-      fieldname: "shift",
-      label: __("Shift Type"),
-      fieldtype: "Link",
-      options: "Shift Type",
-    },
-    {
-      fieldname: "department",
-      label: __("Department"),
-      fieldtype: "Link",
-      options: "Department",
-    },
-    {
-      fieldname: "company",
-      label: __("Company"),
-      fieldtype: "Link",
-      options: "Company",
-    },
-  ],
-  formatter: (value, row, column, data, default_formatter) => {
-    value = default_formatter(value, row, column, data);
-    if (
-      (column.fieldname === "in_time" && data.late_entry) ||
-      (column.fieldname === "out_time" && data.early_exit)
-    ) {
-      value = `<span style='color:red!important'>${value}</span>`;
-    }
-    return value;
-  },
+	filters: [
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+		},
+		{
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+		},
+		{
+			fieldname: "employee",
+			label: __("Employee"),
+			fieldtype: "Link",
+			options: "Employee",
+		},
+		{
+			fieldname: "shift",
+			label: __("Shift Type"),
+			fieldtype: "Link",
+			options: "Shift Type",
+		},
+		{
+			fieldname: "department",
+			label: __("Department"),
+			fieldtype: "Link",
+			options: "Department",
+		},
+		{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+		},
+	],
+	formatter: (value, row, column, data, default_formatter) => {
+		value = default_formatter(value, row, column, data);
+		if (
+			(column.fieldname === "in_time" && data.late_entry) ||
+			(column.fieldname === "out_time" && data.early_exit)
+		) {
+			value = `<span style='color:red!important'>${value}</span>`;
+		}
+		return value;
+	},
 };
