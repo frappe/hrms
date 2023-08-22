b7b61b4ba13d86061bc3d2d44e8cdd30dc0061d7
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-08-22 12:08:59 +0530

    patch: fix fieldname

--- hrms/patches/v15_0/update_shift_type_enable_late_entry_early_exit_marking.py
+++ hrms/patches/v15_0/update_shift_type_enable_late_entry_early_exit_marking.py
@@ -3,7 +3,7 @@ from frappe.model.utils.rename_field import rename_field
 
 def execute():
 	try:
-		rename_field("Shift Type", "enable_entry_grace_period", "enable_entry_grace_period")
+		rename_field("Shift Type", "enable_entry_grace_period", "enable_late_entry_marking")
 		rename_field("Shift Type", "enable_exit_grace_period", "enable_early_exit_marking")
 
 	except Exception as e:
