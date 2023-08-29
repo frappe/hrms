35f3000c6ad7e40a5736bf955362b05fdeb60850
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-08-29 18:02:39 +0530

    feat: add PWA Notification doctype

---
+++ hrms/hr/doctype/pwa_notification/pwa_notification.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+from frappe.model.document import Document
+
+
+class PWANotification(Document):
+	pass
