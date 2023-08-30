43208db0affd2701832ab09b28a85a832fdd7ee8
krantheman <akash.akasht.tom@gmail.com>
2023-08-30 19:45:23 +0530

    fix: losing sort from the params while using input fields
    (cherry picked from commit 030d2c636ea85d039c4dc16010bced9a37dc1506)

--- hrms/www/jobs/index.js
+++ hrms/www/jobs/index.js
@@ -1,48 +1,62 @@
 $(() => {
 	const query_params = frappe.utils.get_query_params();
-	show_applied_filters(query_params);
+	show_applied_filters();
+
 	$("input:checkbox").change(function () {
-		const filters = $("input").serialize();
-		scroll_up_and_update_params(filters);
+		scroll_up_and_update_params(get_new_params());
 	});
+
 	$("#clear-filters").on("click", function () {
 		scroll_up_and_update_params();
 	});
+
 	$("#search-box").bind("search", function () {
-		const filters = $("input").serialize();
-		scroll_up_and_update_params(filters);
+		scroll_up_and_update_params(get_new_params());
 	});
+
 	$("#search-box").keyup(function (e) {
 		if (e.keyCode == 13) {
 			$(this).trigger("search");
 		}
 	});
+
 	$("[name=sort]").on("click", function () {
 		const filters = $("input").serialize();
 		const sort = $.param({ sort: $(this).text() });
 		scroll_up_and_update_params(filters + "&" + sort);
 	});
-});
 
-function show_applied_filters(query_params) {
-	for (const filter in query_params) {
-		if (filter === "query") {
-			$("#search-box").val(query_params["query"]);
-		} else if (filter === "query=") {
-			continue;
-		}
-		if (typeof query_params[filter] === "string") {
-			$("#" + $.escapeSelector(query_params[filter])).prop("checked", true);
-		} else {
-			for (const d of query_params[filter])
-				$("#" + $.escapeSelector(d)).prop("checked", true);
+	function show_applied_filters() {
+		for (const filter in query_params) {
+			if (filter === "query") {
+				$("#search-box").val(query_params["query"]);
+			} else if (
+				["company", "department", "location", "employment_type"].includes(
+					filter
+				)
+			) {
+				if (typeof query_params[filter] === "string") {
+					$("#" + $.escapeSelector(query_params[filter])).prop("checked", true);
+				} else {
+					for (const d of query_params[filter])
+						$("#" + $.escapeSelector(d)).prop("checked", true);
+				}
+			} else {
+				continue;
+			}
 		}
 	}
-}
 
-function scroll_up_and_update_params(filters = "") {
+	function get_new_params() {
+		return "sort" in query_params
+			? $("input").serialize() + "&" + $.param({ sort: query_params["sort"] })
+			: $("input").serialize();
+	}
+});
+
+function scroll_up_and_update_params(params = "") {
 	if (window.scrollY === 0) {
-		window.location.href = "/jobs?" + filters;
+		window.location.href = "/jobs?" + params;
 	} else {
 		window.scroll({
 			top: 0,
@@ -50,7 +64,7 @@ function scroll_up_and_update_params(filters = "") {
 		});
 		window.addEventListener("scroll", function () {
 			if (window.scrollY === 0) {
-				window.location.href = "/jobs?" + filters;
+				window.location.href = "/jobs?" + params;
 			}
 		});
 	}
