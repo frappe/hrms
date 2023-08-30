78981e318a64b45020560a3e838db2e43fc0f148
krantheman <akash.akasht.tom@gmail.com>
2023-08-30 17:18:00 +0530

    feat: add sort by

--- hrms/www/jobs/index.js
+++ hrms/www/jobs/index.js
@@ -1,32 +1,36 @@
 $(() => {
-	show_applied_filters();
+	const query_params = frappe.utils.get_query_params();
+	show_applied_filters(query_params);
 	$("input:checkbox").change(function () {
 		const filters = $("input").serialize();
-		scroll_up_and_update_filters(filters);
+		scroll_up_and_update_params(filters);
 	});
 	$("#clear-filters").on("click", function () {
-		scroll_up_and_update_filters();
+		scroll_up_and_update_params();
 	});
 	$("#search-box").bind("search", function () {
 		const filters = $("input").serialize();
-		scroll_up_and_update_filters(filters);
+		scroll_up_and_update_params(filters);
 	});
 	$("#search-box").keyup(function (e) {
 		if (e.keyCode == 13) {
 			$(this).trigger("search");
 		}
 	});
+	$("[name=sort]").on("click", function () {
+		const filters = $("input").serialize();
+		const sort = $.param({ sort: $(this).text() });
+		scroll_up_and_update_params(filters + "&" + sort);
+	});
 });
 
-function show_applied_filters() {
-	const query_params = frappe.utils.get_query_params();
-	if ("query" in query_params) {
-		$("#search-box").val(query_params["query"]);
-		delete query_params["query"];
-	} else if ("query=" in query_params) {
-		delete query_params["query="];
-	}
+function show_applied_filters(query_params) {
 	for (const filter in query_params) {
+		if (filter === "query") {
+			$("#search-box").val(query_params["query"]);
+		} else if (filter === "query=") {
+			continue;
+		}
 		if (typeof query_params[filter] === "string") {
 			$("#" + $.escapeSelector(query_params[filter])).prop("checked", true);
 		} else {
@@ -36,7 +40,7 @@ function show_applied_filters() {
 	}
 }
 
-function scroll_up_and_update_filters(filters = "") {
+function scroll_up_and_update_params(filters = "") {
 	if (window.scrollY === 0) {
 		window.location.href = "/jobs?" + filters;
 	} else {
