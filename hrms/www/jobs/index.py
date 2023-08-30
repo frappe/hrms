a1624e7f243577ed5ea97b5118250061fb44b472
krantheman <akash.akasht.tom@gmail.com>
2023-08-30 17:18:00 +0530

    feat: add sort by
    (cherry picked from commit 78981e318a64b45020560a3e838db2e43fc0f148)

--- hrms/www/jobs/index.py
+++ hrms/www/jobs/index.py
@@ -5,12 +5,13 @@ from frappe.query_builder.functions import Count
 
 def get_context(context):
 	context.parents = [{"name": _("My Account"), "route": "/"}]
-	applied_filters = frappe.request.args.to_dict(flat=False)
-	context.job_openings = get_job_openings(applied_filters)
-	context.filters = get_all_filters(applied_filters)
+	args = frappe.request.args.to_dict(flat=False)
+	filters, txt, context.order_by = get_filters_txt_and_order_by(args)
+	context.job_openings = get_job_openings(filters, txt, context.order_by)
+	context.all_filters = get_all_filters(filters)
 
 
-def get_job_openings(applied_filters=None, limit_start=0, limit_page_length=20, order_by=None):
+def get_job_openings(filters=None, txt=None, order_by=None, limit_start=0, limit_page_length=20):
 
 	jo = frappe.qb.DocType("Job Opening")
 	ja = frappe.qb.DocType("Job Applicant")
@@ -42,35 +43,52 @@ def get_job_openings(applied_filters=None, limit_start=0, limit_page_length=20, 
 		.groupby(jo.name)
 	)
 
-	if "query" in applied_filters:
-		search = f"%{applied_filters['query'][0]}%"
-		query = query.where((jo.job_title.like(search)) | (jo.description.like(search)))
-		del applied_filters["query"]
+	for d in filters:
+		query = query.where(frappe.qb.Field(d).isin(filters[d]))
 
-	for d in applied_filters:
-		query = query.where(frappe.qb.Field(d).isin(applied_filters[d]))
+	if txt:
+		query = query.where((jo.job_title.like(f"%{txt}%")) | (jo.description.like(f"%{txt}%")))
+
+	if order_by:
+		print(order_by)
 
 	return query.run(as_dict=True)
 
 
-def get_all_filters(applied_filters=None):
+def get_all_filters(filters=None):
 	job_openings = frappe.get_all(
 		"Job Opening",
 		fields=["company", "department", "location", "employment_type"],
 	)
 
-	companies = applied_filters["company"] if "company" in applied_filters else None
+	companies = filters["company"] if "company" in filters else None
 
-	filters = {}
+	all_filters = {}
 	for d in job_openings:
 		for key, value in d.items():
 			if key == "company" or not companies or (companies and d["company"] in companies):
-				if key not in filters:
-					filters[key] = [value]
-				elif value and value not in filters[key]:
-					filters[key].append(value)
+				if key not in all_filters:
+					all_filters[key] = [value]
+				elif value and value not in all_filters[key]:
+					all_filters[key].append(value)
 
-	for d in filters:
-		filters[d].sort()
+	for d in all_filters:
+		all_filters[d].sort()
 
-	return filters
+	return all_filters
+
+
+def get_filters_txt_and_order_by(args):
+	filters = {}
+	txt = ""
+	order_by = "Newest Post"
+	allowed_filters = ["company", "department", "location", "employment_type"]
+	for d in args:
+		if d in allowed_filters:
+			filters[d] = args[d]
+		elif d == "query":
+			txt = args["query"][0]
+		elif d == "sort":
+			if args["sort"][0] in ["Oldest Post", "Earliest Closing", "Latest Closing"]:
+				order_by = args["sort"][0]
+	return filters, txt, order_by
