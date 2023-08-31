b745e976b9725031b4884ea94175da67d092d5d7
krantheman <akash.akasht.tom@gmail.com>
2023-08-31 12:28:18 +0530

    feat: add sorting functionality
    (cherry picked from commit 8eab161d8aea58d7e0dcf2fb85472d4ee3872197)

--- hrms/www/jobs/index.py
+++ hrms/www/jobs/index.py
@@ -1,12 +1,20 @@
 import frappe
 from frappe import _
+from frappe.query_builder import Order
 from frappe.query_builder.functions import Count
 
 
 def get_context(context):
 	context.parents = [{"name": _("My Account"), "route": "/"}]
-	filters, txt, order_by = get_filters_txt_and_order_by()
-	context.job_openings = get_job_openings(filters, txt, order_by)
+	orders = {
+		"Recent Posts": ["posted_on", Order.desc],
+		"Early Posts": ["posted_on", Order.asc],
+		"Urgent Deadlines": ["closes_on", Order.asc],
+		"Extended Deadlines": ["closes_on", Order.desc],
+	}
+	filters, txt, order_by = get_filters_txt_and_order_by(list(orders.keys()))
+	context.orders = orders.keys()
+	context.job_openings = get_job_openings(filters, txt, orders[order_by])
 	context.all_filters = get_all_filters(filters)
 	context.order_by = order_by
 
@@ -50,7 +58,7 @@ def get_job_openings(filters=None, txt=None, order_by=None, limit_start=0, limit
 		query = query.where((jo.job_title.like(f"%{txt}%")) | (jo.description.like(f"%{txt}%")))
 
 	if order_by:
-		print(order_by)
+		query = query.orderby(order_by[0], order=order_by[1])
 
 	return query.run(as_dict=True)
 
@@ -78,11 +86,11 @@ def get_all_filters(filters=None):
 	return all_filters
 
 
-def get_filters_txt_and_order_by():
+def get_filters_txt_and_order_by(orders):
 	args = frappe.request.args.to_dict(flat=False)
 	filters = {}
 	txt = ""
-	order_by = "Newest Post"
+	order_by = orders[0]
 	allowed_filters = ["company", "department", "location", "employment_type"]
 
 	for d in args:
@@ -91,6 +99,6 @@ def get_filters_txt_and_order_by():
 		elif d == "query":
 			txt = args["query"][0]
 		elif d == "sort":
-			if args["sort"][0] in ["Oldest Post", "Earliest Closing", "Latest Closing"]:
+			if args["sort"][0] in orders:
 				order_by = args["sort"][0]
 	return filters, txt, order_by
