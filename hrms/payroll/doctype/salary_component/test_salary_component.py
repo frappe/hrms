a1241e7de230110585f2d22d4976f2afedc15844
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-09-01 22:01:56 +0530

    fix(Payroll JE): link employee & advance reference in advance return entries

--- hrms/payroll/doctype/salary_component/test_salary_component.py
+++ hrms/payroll/doctype/salary_component/test_salary_component.py
@@ -12,12 +12,14 @@ class TestSalaryComponent(FrappeTestCase):
 
 
 def create_salary_component(component_name, **args):
-	if not frappe.db.exists("Salary Component", component_name):
-		frappe.get_doc(
-			{
-				"doctype": "Salary Component",
-				"salary_component": component_name,
-				"type": args.get("type") or "Earning",
-				"is_tax_applicable": args.get("is_tax_applicable") or 1,
-			}
-		).insert()
+	if frappe.db.exists("Salary Component", component_name):
+		return frappe.get_doc("Salary Component", component_name)
+
+	return frappe.get_doc(
+		{
+			"doctype": "Salary Component",
+			"salary_component": component_name,
+			"type": args.get("type") or "Earning",
+			"is_tax_applicable": args.get("is_tax_applicable") or 1,
+		}
+	).insert()
