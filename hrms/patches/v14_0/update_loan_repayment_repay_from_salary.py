3e32e725e3aead1177c7b0f8dcbb06fbb0800a13
anandbaburajan <anandbaburajan@gmail.com>
2023-09-18 16:16:53 +0530

    fix: fetch logic for repay_from_salary in loan_repayment

--- hrms/patches/v14_0/update_loan_repayment_repay_from_salary.py
+++ hrms/patches/v14_0/update_loan_repayment_repay_from_salary.py
@@ -4,5 +4,7 @@ import frappe
 def execute():
 	if frappe.db.exists("Custom Field", {"name": "Loan Repayment-repay_from_salary"}):
 		frappe.db.set_value(
-			"Custom Field", {"name": "Loan Repayment-repay_from_salary"}, { "fetch_from": None, "fetch_if_empty": 0 }
+			"Custom Field",
+			{"name": "Loan Repayment-repay_from_salary"},
+			{"fetch_from": None, "fetch_if_empty": 0},
 		)
