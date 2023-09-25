0b372c2744fa79c58bbc36cd377a713ef45ca906
Francisco Roldán <franciscoproldan@gmail.com>
2023-09-25 02:08:01 -0300

    fix: required_apps (#911)
    (cherry picked from commit 4f4bbe0c971d0984f9653228906af47be06edcc5)

--- hrms/hooks.py
+++ hrms/hooks.py
@@ -4,7 +4,7 @@ app_publisher = "Frappe Technologies Pvt. Ltd."
 app_description = "Modern HR and Payroll Software"
 app_email = "contact@frappe.io"
 app_license = "GNU General Public License (v3)"
-required_apps = ["erpnext"]
+required_apps = ["frappe/erpnext"]
 
 
 # Includes in <head>
