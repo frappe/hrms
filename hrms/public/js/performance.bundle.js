467b3ecedca3f651ea2c9a1bc5e99c78c54a130a
krantheman <akash.akasht.tom@gmail.com>
2023-10-06 12:34:45 +0530

    refactor: use common feedback_history and feedback_summary

--- hrms/public/js/performance.bundle.js
+++ hrms/public/js/performance.bundle.js
@@ -1,4 +1,5 @@
 import "./performance/performance_feedback.js";
 import "./templates/performance_feedback_history.html";
-import "./templates/performance_feedback_summary.html";
+import "./templates/feedback_summary.html";
+import "./templates/feedback_history.html";
 import "./templates/rating.html";
