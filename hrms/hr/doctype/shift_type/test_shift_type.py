d6106a3ae2f85a2efc7d37423efd1e75e9f040b8
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-10-06 20:20:47 +0530

    fix: absent marking fails if shift's end date & actual end date are different
    (cherry picked from commit d01d209b383f7ea5c02fc146f3a1990d7d3db8d8)

--- hrms/hr/doctype/shift_type/test_shift_type.py
+++ hrms/hr/doctype/shift_type/test_shift_type.py
@@ -369,26 +369,19 @@ class TestShiftType(FrappeTestCase):
 		make_shift_assignment(shift_type.name, employee, date1, date1)
 
 		# assignment without end date
-		make_shift_assignment(shift_type.name, employee, add_days(today, -4))
+		date2 = add_days(today, -4)
+		make_shift_assignment(shift_type.name, employee, date2, date2)
 
 		shift_type.process_auto_attendance()
-		yesterday = add_days(today, -1)
-
-		# absentees are auto-marked one day after shift actual end to wait for any manual attendance records
-		# so all days should be marked as absent except today
 		absent_records = frappe.get_all(
 			"Attendance",
 			{
-				"attendance_date": ["between", [date1, yesterday]],
+				"attendance_date": ["between", [date1, today]],
 				"employee": employee,
 				"status": "Absent",
 			},
 		)
-		self.assertEqual(len(absent_records), 5)
-		todays_attendance = frappe.db.get_value(
-			"Attendance", {"attendance_date": today, "employee": employee}
-		)
-		self.assertIsNone(todays_attendance)
+		self.assertEqual(len(absent_records), 2)
 
 	def test_do_not_mark_absent_before_shift_actual_end_time(self):
 		"""
