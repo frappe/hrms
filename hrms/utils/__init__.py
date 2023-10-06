9415f1339b4aaced744dacfecba412b54458bbbe
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-10-07 00:23:20 +0530

    chore: fix conflicts

--- hrms/utils/__init__.py
+++ hrms/utils/__init__.py
@@ -34,8 +34,6 @@ def get_date_range(start_date: str, end_date: str) -> list[str]:
 	"""returns list of dates between start and end dates"""
 	no_of_days = date_diff(end_date, start_date) + 1
 	return [add_days(start_date, i) for i in range(no_of_days)]
-<<<<<<< HEAD
-=======
 
 
 def generate_date_range(
@@ -48,20 +46,3 @@ def generate_date_range(
 
 	for n in range(no_of_days):
 		yield add_days(date_field, direction * n)
-
-
-def get_employee_email(employee_id: str) -> str | None:
-	employee_emails = frappe.db.get_value(
-		"Employee",
-		employee_id,
-		["prefered_email", "user_id", "company_email", "personal_email"],
-		as_dict=True,
-	)
-
-	return (
-		employee_emails.prefered_email
-		or employee_emails.user_id
-		or employee_emails.company_email
-		or employee_emails.personal_email
-	)
->>>>>>> 015eb30f (perf: use generator fn for date range to find prev/next assignment)
