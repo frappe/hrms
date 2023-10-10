e4fbf0c8e320d68841f8a1009ceafa20d1c1ec23
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-10-10 16:59:21 +0530

    fix: disable menu on login page

--- frontend/src/components/Menu.vue
+++ frontend/src/components/Menu.vue
@@ -1,5 +1,9 @@
 <template>
-	<ion-menu side="start" content-id="main-content" :swipe-gesture="false">
+	<ion-menu
+		content-id="main-content"
+		:swipe-gesture="false"
+		:disabled="route.name === 'Login'"
+	>
 		<ion-content class="ion-padding">
 			<div class="bg-white h-full">
 				<div
@@ -18,4 +22,7 @@ import { IonContent, IonMenu } from "@ionic/vue"
 
 import MenuLinks from "@/components/MenuLinks.vue"
 import FrappeHRLogo from "@/components/icons/FrappeHRLogo.vue"
+import { useRoute } from "vue-router"
+
+const route = useRoute()
 </script>
