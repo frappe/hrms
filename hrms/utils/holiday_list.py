d6be0e01ac5c3f8133e76c8a822711869dc21551
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-11-02 21:18:45 +0530

    chore: fix conflicts

--- hrms/utils/holiday_list.py
+++ hrms/utils/holiday_list.py
@@ -13,19 +13,9 @@ def get_holiday_dates_between(
 		.select(Holiday.holiday_date)
 		.where((Holiday.parent == holiday_list) & (Holiday.holiday_date.between(start_date, end_date)))
 		.orderby(Holiday.holiday_date)
-<<<<<<< HEAD
 	).run(pluck=True)
-=======
-	)
 
 	if skip_weekly_offs:
 		query = query.where(Holiday.weekly_off == 0)
 
 	return query.run(pluck=True)
-
-
-def invalidate_cache(doc, method=None):
-	from hrms.payroll.doctype.salary_slip.salary_slip import HOLIDAYS_BETWEEN_DATES
-
-	frappe.cache().delete_value(HOLIDAYS_BETWEEN_DATES)
->>>>>>> e64a5e51d (feat(util): add provision to get holidays except weekly offs from holiday list (#1011))
