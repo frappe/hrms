192d71305b16e238064e4f9d6a242da68ac1ece5
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-11-03 11:53:56 +0530

    feat: add safe-area utils to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,8 +10,14 @@ module.exports = {
 			screens: {
 				standalone: {
 					raw: "(display-mode: standalone)",
-				}
-			}
+				},
+			},
+			padding: {
+				"safe-top": "env(safe-area-inset-top)",
+				"safe-right": "env(safe-area-inset-right)",
+				"safe-bottom": "env(safe-area-inset-bottom)",
+				"safe-left": "env(safe-area-inset-left)",
+			},
 		},
 	},
 	plugins: [],
