abf8d3f0060ffdac4cfb4ace5844b5cc86f71492
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-11-06 21:12:39 +0530

    fix: include private files in proxy config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,14 @@ import vue from "@vitejs/plugin-vue"
 import { VitePWA } from "vite-plugin-pwa"
 
 import path from "path"
-import frappeui from "frappe-ui/vite"
+import fs from "fs"
 
 export default defineConfig({
+	server: {
+		port: 8080,
+		proxy: getProxyOptions(),
+	},
 	plugins: [
-		frappeui(),
 		vue(),
 		VitePWA({
 			registerType: "autoUpdate",
@@ -71,3 +74,41 @@ export default defineConfig({
 		],
 	},
 })
+
+function getProxyOptions() {
+	const config = getCommonSiteConfig()
+	const webserver_port = config ? config.webserver_port : 8000
+	if (!config) {
+		console.log("No common_site_config.json found, using default port 8000")
+	}
+	return {
+		"^/(app|login|api|assets|files|private)": {
+			target: `http://127.0.0.1:${webserver_port}`,
+			ws: true,
+			router: function (req) {
+				const site_name = req.headers.host.split(":")[0]
+				console.log(`Proxying ${req.url} to ${site_name}:${webserver_port}`)
+				return `http://${site_name}:${webserver_port}`
+			},
+		},
+	}
+}
+
+function getCommonSiteConfig() {
+	let currentDir = path.resolve(".")
+	// traverse up till we find frappe-bench with sites directory
+	while (currentDir !== "/") {
+		if (
+			fs.existsSync(path.join(currentDir, "sites")) &&
+			fs.existsSync(path.join(currentDir, "apps"))
+		) {
+			let configPath = path.join(currentDir, "sites", "common_site_config.json")
+			if (fs.existsSync(configPath)) {
+				return JSON.parse(fs.readFileSync(configPath))
+			}
+			return null
+		}
+		currentDir = path.resolve(currentDir, "..")
+	}
+	return null
+}
