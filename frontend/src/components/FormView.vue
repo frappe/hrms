d81309ecab473dc22f57ff9ae68cf4e9778b768b
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-12-01 09:24:22 +0530

    fix: go to prev route on doc deletion

--- frontend/src/components/FormView.vue
+++ frontend/src/components/FormView.vue
@@ -654,6 +654,7 @@ function submitOrCancelForm() {
 function handleDocDelete() {
 	documentResource.delete.submit()
 	showDeleteDialog.value = false
+	router.back()
 }
 
 async function reloadDoc() {
