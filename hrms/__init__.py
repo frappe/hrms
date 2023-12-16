3dfb395c2a7a352d026edeebf92240059eececf8
Rucha Mahabal <ruchamahabal2@gmail.com>
2023-12-16 21:41:41 +0530

    feat(PWA): socket setup - add `refetch_resource` listener
    (cherry picked from commit 728cae8e4a22a0fb29109952d7651ab04a2ba5b8)
    
    # Conflicts:
    #	hrms/__init__.py

--- hrms/__init__.py
+++ hrms/__init__.py
@@ -1,1 +1,16 @@
+<<<<<<< HEAD
 __version__ = "15.0.0"
+=======
+import frappe
+
+__version__ = "16.0.0-dev"
+
+
+def refetch_resource(cache_key: str | list, user=None):
+	frappe.publish_realtime(
+		"hrms:refetch_resource",
+		{"cache_key": cache_key},
+		user=user or frappe.session.user,
+		after_commit=True,
+	)
+>>>>>>> 728cae8e4 (feat(PWA): socket setup - add `refetch_resource` listener)
