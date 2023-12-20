60ccf5c66c3e2ae1d9a7f9e1866a361e30ba26b1
Vishnu  VS <Vishnuviswambaran2002@gmail.com>
2023-12-20 23:03:42 +0530

    Update hrms/hr/doctype/attendance_request/attendance_request.py
    Co-authored-by: Rucha Mahabal <ruchamahabal2@gmail.com>

--- hrms/hr/doctype/attendance_request/attendance_request.py
+++ hrms/hr/doctype/attendance_request/attendance_request.py
@@ -118,7 +118,7 @@ class AttendanceRequest(Document):
 
 	def should_mark_attendance(self, attendance_date: str) -> bool:
 		# Check if attendance_date is a holiday
-		if not self.ignore_holiday and is_holiday(self.employee, attendance_date):
+		if not self.include_holidays and is_holiday(self.employee, attendance_date):
 			frappe.msgprint(
 				_("Attendance not submitted for {0} as it is a Holiday.").format(
 					frappe.bold(format_date(attendance_date))
