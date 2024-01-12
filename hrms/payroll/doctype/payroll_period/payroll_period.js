e91def28e9ed518f0d61b83e1ca544c5a1851914
krantheman <akash.akasht.tom@gmail.com>
2024-01-12 13:24:54 +0530

    feat(Create Payroll Period): set Start & End Date as that of Fiscal Year if no Payroll Period exists
    (cherry picked from commit a3060da3d5d30af939d77d46210e7f3d440e1a17)
    
    # Conflicts:
    #	hrms/payroll/doctype/payroll_period/payroll_period.js

--- hrms/payroll/doctype/payroll_period/payroll_period.js
+++ hrms/payroll/doctype/payroll_period/payroll_period.js
@@ -2,5 +2,20 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Payroll Period", {
+<<<<<<< HEAD
 	refresh: function (frm) {},
+=======
+	refresh: function (frm) {
+		// sets start date and end date as that of the current fiscal year if no payroll period exists
+		frappe.db.exists("Payroll Period").then((period_exists) => {
+			if (!period_exists) {
+				frm.set_value(
+					"start_date",
+					frappe.defaults.get_default("year_start_date")
+				);
+				frm.set_value("end_date", frappe.defaults.get_default("year_end_date"));
+			}
+		});
+	},
+>>>>>>> a3060da3d (feat(Create Payroll Period): set Start & End Date as that of Fiscal Year if no Payroll Period exists)
 });
