449d3258678f4ad00d77e7dce87092853d378efe
KerollesFathy <kerolles.f@outlook.com>
2024-01-19 16:46:45 +0200

    fix: get income tax slabs by getting ss_assignment base on from_date

--- hrms/payroll/doctype/salary_slip/salary_slip.py
+++ hrms/payroll/doctype/salary_slip/salary_slip.py
@@ -1393,8 +1393,9 @@ class SalarySlip(TransactionBase):
 	def get_income_tax_slabs(self):
 		income_tax_slab, ss_assignment_name = frappe.db.get_value(
 			"Salary Structure Assignment",
-			{"employee": self.employee, "salary_structure": self.salary_structure, "docstatus": 1},
+			{"employee": self.employee, "salary_structure": self.salary_structure, "docstatus": 1, "from_date": ("<=",  self.end_date)},
 			["income_tax_slab", "name"],
+			order_by="from_date desc",
 		)
 
 		if not income_tax_slab:
