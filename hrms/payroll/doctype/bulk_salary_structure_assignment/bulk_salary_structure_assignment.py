105a97d1eb94402275f77c2833e2971138e57d7d
krantheman <akash.akasht.tom@gmail.com>
2024-01-29 16:10:23 +0530

    feat: add Quick and Advanced Filters
    (cherry picked from commit 2e77bba5bc2bbd8c03213c06c401edda68a6cf43)

--- hrms/payroll/doctype/bulk_salary_structure_assignment/bulk_salary_structure_assignment.py
+++ hrms/payroll/doctype/bulk_salary_structure_assignment/bulk_salary_structure_assignment.py
@@ -1,9 +1,40 @@
 # Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.query_builder.terms import SubQuery
 
 
 class BulkSalaryStructureAssignment(Document):
-	pass
+	@frappe.whitelist()
+	def get_employees(self, advanced_filters: list) -> list:
+		Assignment = frappe.qb.DocType("Salary Structure Assignment")
+		employees_with_assignments = (
+			frappe.qb.from_(Assignment)
+			.select(Assignment.employee)
+			.distinct()
+			.where((Assignment.from_date == self.from_date) & (Assignment.docstatus == 1))
+			.run(pluck=True)
+		)
+
+		quick_filter_fields = [
+			"company",
+			"employment_type",
+			"branch",
+			"department",
+			"designation",
+			"grade",
+		]
+		quick_filters = [[d, "=", self.get(d)] for d in quick_filter_fields if self.get(d)]
+
+		filters = (
+			[["status", "=", "Active"], ["employee", "not in", employees_with_assignments]]
+			+ quick_filters
+			+ advanced_filters
+		)
+		return frappe.get_list(
+			"Employee",
+			filters=filters,
+			fields=["employee", "employee_name", "company", "department"],
+		)
