80e6193841aad37faa0df0d02145738728803cea
Ankush Menat <ankush@frappe.io>
2024-02-06 14:23:31 +0530

    fix: change server-action args (#1398)
    We accept flat argument instead of `args` now. Ref: https://github.com/frappe/frappe/pull/24782

--- hrms/hr/doctype/interview/interview.py
+++ hrms/hr/doctype/interview/interview.py
@@ -187,18 +187,13 @@ def get_skill_wise_average_rating(interview: str) -> list[dict]:
 
 
 @frappe.whitelist()
-def update_job_applicant_status(args):
-	import json
-
+def update_job_applicant_status(status: str, job_applicant: str):
 	try:
-		if isinstance(args, str):
-			args = json.loads(args)
-
-		if not args.get("job_applicant"):
+		if not job_applicant:
 			frappe.throw(_("Please specify the job applicant to be updated."))
 
-		job_applicant = frappe.get_doc("Job Applicant", args["job_applicant"])
-		job_applicant.status = args["status"]
+		job_applicant = frappe.get_doc("Job Applicant", job_applicant)
+		job_applicant.status = status
 		job_applicant.save()
 
 		frappe.msgprint(
