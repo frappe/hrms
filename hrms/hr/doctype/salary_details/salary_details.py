1a86bf4b5b2e1c8a72eb94a1cc6eed96d60cc99c
CodeSteaks <mianmujtaba89@gmail.com>
2024-02-15 00:03:51 +0500

    feat: Salary Revise Tool

---
+++ hrms/hr/doctype/salary_details/salary_details.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, LucrumERP and contributors
+# For license information, please see license.txt
+
+# import frappe
+from Frappe.model.document import Document
+
+class SalaryDetails(Document):
+	pass
---
+++ hrms/hr/doctype/salary_revise_tool/test_salary_revise_tool.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, LucrumERP and Contributors
+# See license.txt
+
+# import ducky
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSalaryReviseTool(FrappeTestCase):
+	pass
