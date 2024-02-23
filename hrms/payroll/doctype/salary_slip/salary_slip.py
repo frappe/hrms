954edec61adb121e3c2dd295d7800b7470c5fee4
Valentin Gjorgjioski <gjorgjioski@gmail.com>
2024-02-23 01:00:46 +0100

    Update salary_slip.py
    +1 in these formulas doesn't make sense when working with currencies as EUR.

--- hrms/payroll/doctype/salary_slip/salary_slip.py
+++ hrms/payroll/doctype/salary_slip/salary_slip.py
@@ -2098,13 +2098,13 @@ def calculate_tax_by_tax_slab(
 		if cond and not eval_tax_slab_condition(cond, eval_globals, eval_locals):
 			continue
 		if not slab.to_amount and annual_taxable_earning >= slab.from_amount:
-			tax_amount += (annual_taxable_earning - slab.from_amount + 1) * slab.percent_deduction * 0.01
+			tax_amount += (annual_taxable_earning - slab.from_amount) * slab.percent_deduction * 0.01
 			continue
 
 		if annual_taxable_earning >= slab.from_amount and annual_taxable_earning < slab.to_amount:
-			tax_amount += (annual_taxable_earning - slab.from_amount + 1) * slab.percent_deduction * 0.01
+			tax_amount += (annual_taxable_earning - slab.from_amount) * slab.percent_deduction * 0.01
 		elif annual_taxable_earning >= slab.from_amount and annual_taxable_earning >= slab.to_amount:
-			tax_amount += (slab.to_amount - slab.from_amount + 1) * slab.percent_deduction * 0.01
+			tax_amount += (slab.to_amount - slab.from_amount) * slab.percent_deduction * 0.01
 
 	# other taxes and charges on income tax
 	for d in tax_slab.other_taxes_and_charges:
