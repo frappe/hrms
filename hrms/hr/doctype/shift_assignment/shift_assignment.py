5731c8fa277dd0f9bfb30241f06ad4ab38dd8a49
krantheman <akash.akasht.tom@gmail.com>
2024-02-27 16:42:08 +0530

    fix(Shift Assignment): calendar view date filter

--- hrms/hr/doctype/shift_assignment/shift_assignment.py
+++ hrms/hr/doctype/shift_assignment/shift_assignment.py
@@ -171,7 +171,7 @@ def get_shift_assignments(start: str, end: str, filters: str | list | None = Non
 	if not filters:
 		filters = []
 
-	filters.extend([["start_date", ">=", start], ["end_date", "<=", end], ["docstatus", "=", 1]])
+	filters.extend([["start_date", "<=", end], ["end_date", ">=", start], ["docstatus", "=", 1]])
 
 	return frappe.get_list(
 		"Shift Assignment",
