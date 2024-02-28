08131d63a31e93bfa59ee94ea359d03f8c701bb0
Rucha Mahabal <ruchamahabal2@gmail.com>
2024-02-28 20:11:52 +0530

    feat: set relay server URL and push notification relay status in boot context
    (cherry picked from commit c62ea24be48a596373eba2e831e8b70f62b8d310)

--- hrms/www/hrms.py
+++ hrms/www/hrms.py
@@ -6,4 +6,25 @@ no_cache = 1
 def get_context(context):
 	csrf_token = frappe.sessions.get_csrf_token()
 	frappe.db.commit()  # nosempgrep
-	context.csrf_token = csrf_token
+	context = frappe._dict()
+	context.boot = get_boot()
+	context.boot.csrf_token = csrf_token
+	return context
+
+
+@frappe.whitelist(methods=["POST"], allow_guest=True)
+def get_context_for_dev():
+	if not frappe.conf.developer_mode:
+		frappe.throw("This method is only meant for developer mode")
+	return get_boot()
+
+
+def get_boot():
+	return frappe._dict(
+		{
+			"push_notifications_enabled": frappe.db.get_single_value(
+				"Push Notification Settings", "enable_push_notification_relay"
+			),
+			"push_relay_server_url": frappe.conf.get("push_relay_server_url"),
+		}
+	)
