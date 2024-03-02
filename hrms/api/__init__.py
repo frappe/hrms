f6ffe47403961a66e1eca0d3d0962c49c3766caa
Rucha Mahabal <ruchamahabal2@gmail.com>
2024-03-03 03:24:18 +0530

    fix: handle incompatible frappe version

--- hrms/api/__init__.py
+++ hrms/api/__init__.py
@@ -97,7 +97,11 @@ def mark_all_notifications_as_read() -> None:
 
 @frappe.whitelist()
 def are_push_notifications_enabled() -> bool:
-	return frappe.db.get_single_value("Push Notification Settings", "enable_push_notification_relay")
+	try:
+		return frappe.db.get_single_value("Push Notification Settings", "enable_push_notification_relay")
+	except frappe.DoesNotExistError:
+		# push notifications are not supported in the current framework version
+		return False
 
 
 # Leaves and Holidays
