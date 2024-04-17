26ed6020cb87c1ee3607e8b577ef82962904de3c
krantheman <akash.akasht.tom@gmail.com>
2024-04-17 16:49:51 +0530

    feat: add Shift Assignment Tool button to Shift Assignment List View and Shift Type Form View
    (cherry picked from commit 8b54c49b500709ff5a04702fa4ca8aa396d65b7c)

---
+++ hrms/hr/doctype/shift_assignment/shift_assignment_list.js
@@ -0,0 +1,7 @@
+frappe.listview_settings["Shift Assignment"] = {
+	onload: function (list_view) {
+		list_view.page.add_inner_button(__("Shift Assignment Tool"), function () {
+			frappe.set_route("Form", "Shift Assignment Tool");
+		});
+	},
+};
