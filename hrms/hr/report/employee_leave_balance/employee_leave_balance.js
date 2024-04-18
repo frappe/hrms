92ff23112676bb73de458f61cf76997c422f6fa9
Corentin Flr <10946971+cogk@users.noreply.github.com>
2024-04-18 15:56:34 +0200

    fix(Leave Balance): Add context to translation for "Left" (Employee)

--- hrms/hr/report/employee_leave_balance/employee_leave_balance.js
+++ hrms/hr/report/employee_leave_balance/employee_leave_balance.js
@@ -46,7 +46,7 @@ frappe.query_reports["Employee Leave Balance"] = {
 				{ value: "Active", label: __("Active") },
 				{ value: "Inactive", label: __("Inactive") },
 				{ value: "Suspended", label: __("Suspended") },
-				{ value: "Left", label: __("Left") },
+				{ value: "Left", label: __("Left", null, "Employee") },
 			],
 			default: "Active",
 		},
