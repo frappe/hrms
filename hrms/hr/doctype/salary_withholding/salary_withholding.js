baf4a8c6bcf8430d154081202745afaa0258b517
Viny Selopal <viny0698@gmail.com>
2024-04-26 12:06:51 +0530

    feat(salary withhold): autofill to_date field on changing from_date

--- hrms/hr/doctype/salary_withholding/salary_withholding.js
+++ hrms/hr/doctype/salary_withholding/salary_withholding.js
@@ -12,11 +12,47 @@ frappe.ui.form.on("Salary Withholding", {
 				else {
 					const relieving_date = frappe.datetime.add_months(frappe.datetime.get_today(), months = doc.notice)
 					frm.doc.relieving_date = relieving_date
-
 				}
 				refresh_field("date_of_joining")
 				refresh_field("relieving_date")
 			})
 		}
 	},
+	from_date: function (frm) {
+		if (frm.doc.number_of_withholding_cycles) {
+			from_date = frm.doc.from_date
+			frappe.db.get_list("Salary Structure Assignment", {
+				filters: {
+					employee: frm.doc.employee
+				},
+				fields: ["salary_structure"],
+				order_by: "from_date desc"
+			}).then((data) => {
+				frappe.db.get_value("Salary Structure", data[0].salary_structure, "payroll_frequency").then((r) => {
+					switch (r.message.payroll_frequency) {
+						case "Monthly":
+							console.log("monthly")
+							frm.doc.to_date = frappe.datetime.add_months(from_date, months = frm.doc.number_of_withholding_cycles)
+							break
+						case "Bimonthly":
+							console.log("bimonthly")
+							frm.doc.to_date = frappe.datetime.add_months(from_date, months = frm.doc.number_of_withholding_cycles * 2)
+							break
+						case "Weekly":
+							console.log("weekly")
+							frm.doc.to_date = frappe.datetime.add_days(from_date, days = frm.doc.number_of_withholding_cycles * 7)
+							break
+						case "Fortnightly":
+							console.log("fortnightly")
+							frm.doc.to_date = frappe.datetime.add_days(from_date, days = frm.doc.number_of_withholding_cycles * 14)
+							break
+						case "Daily":
+							console.log("daily")
+							frm.doc.to_date = frappe.datetime.add_days(from_date, days = frm.doc.number_of_withholding_cycles)
+					}
+					refresh_field("to_date")
+				})
+			})
+		}
+	}
 });
