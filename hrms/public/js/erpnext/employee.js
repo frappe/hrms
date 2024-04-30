a590b19e559220f9fa8819ac85196e05bd046136
Viny Selopal <viny0698@gmail.com>
2024-04-30 15:38:30 +0530

    feat(salary withhold): show "Create Salary Withholding Document" button in Employee form

--- hrms/public/js/erpnext/employee.js
+++ hrms/public/js/erpnext/employee.js
@@ -3,7 +3,7 @@
 
 frappe.ui.form.on("Employee", {
 	refresh: function (frm) {
-		frm.set_query("payroll_cost_center", function() {
+		frm.set_query("payroll_cost_center", function () {
 			return {
 				filters: {
 					"company": frm.doc.company,
@@ -11,8 +11,24 @@ frappe.ui.form.on("Employee", {
 				}
 			};
 		});
-	},
 
+		if (!frm.doc.resignation_letter_date) return
+
+		frappe.db.get_list("Salary Withholding", {
+			filters: {
+				"employee": frm.doc.name
+			}
+		}).then((docs) => {
+			if (!docs[0]) {
+				frm.add_custom_button(__("Create Salary Withholding Document"), () => {
+					frappe.route_options = {
+						employee: frm.doc.name,
+					};
+					frappe.set_route("Form", "Salary Withholding");
+				})
+			}
+		})
+	},
 	date_of_birth(frm) {
 		frm.call({
 			method: "hrms.overrides.employee_master.get_retirement_date",
