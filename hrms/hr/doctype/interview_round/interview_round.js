d1949e0682939566e43f36badb3697f811b75b3d
Rucha Mahabal <ruchamahabal2@gmail.com>
2024-05-01 12:19:20 +0530

    fix: move designation field above skillset to convey association

--- hrms/hr/doctype/interview_round/interview_round.js
+++ hrms/hr/doctype/interview_round/interview_round.js
@@ -10,17 +10,15 @@ frappe.ui.form.on("Interview Round", {
 		}
 	},
 	designation: function (frm) {
-		frm.set_value("expected_skill_set", null);
 		if (frm.doc.designation) {
 			frappe.db.get_doc("Designation", frm.doc.designation).then((designation) => {
+				frappe.model.clear_table(frm.doc, "expected_skill_set");
+
 				designation.skills.forEach((designation_skill) => {
-					let row = frappe.model.add_child(
-						frm.doc,
-						"Expected Skill Set",
-						"expected_skill_set",
-					);
+					const row = frm.add_child("expected_skill_set");
 					row.skill = designation_skill.skill;
 				});
+
 				refresh_field("expected_skill_set");
 			});
 		}
