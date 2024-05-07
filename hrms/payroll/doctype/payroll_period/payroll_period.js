15c8163000049ca57244f120a6e137e0ecce1bf4
Rucha Mahabal <ruchamahabal2@gmail.com>
2024-05-07 17:48:36 +0530

    fix: set payroll period dates based on last payroll period/current fiscal year
    - disable quick entry for payroll period to make the form scripts work
    
    (cherry picked from commit 22a9000b370ecc28dafdc9600e1dd8679407675b)
    
    # Conflicts:
    #	hrms/payroll/doctype/payroll_period/payroll_period.js
    #	hrms/payroll/doctype/payroll_period/payroll_period.json

--- hrms/payroll/doctype/payroll_period/payroll_period.js
+++ hrms/payroll/doctype/payroll_period/payroll_period.js
@@ -3,6 +3,7 @@
 
 frappe.ui.form.on("Payroll Period", {
 <<<<<<< HEAD
+<<<<<<< HEAD
 	refresh: function (frm) {},
 =======
 	refresh: function (frm) {
@@ -13,6 +14,38 @@ frappe.ui.form.on("Payroll Period", {
 				frm.set_value("end_date", frappe.defaults.get_default("year_end_date"));
 			}
 		});
+=======
+	onload: function (frm) {
+		frm.trigger("set_start_date");
+	},
+
+	set_start_date: function (frm) {
+		if (!frm.doc.__islocal) return;
+
+		frappe.db
+			.get_list("Payroll Period", {
+				fields: ["end_date"],
+				order_by: "end_date desc",
+				limit: 1,
+			})
+			.then((result) => {
+				// set start date based on end date of the last payroll period if found
+				// else set it based on the current fiscal year's start date
+				if (result.length) {
+					const last_end_date = result[0].end_date;
+					frm.set_value("start_date", frappe.datetime.add_days(last_end_date, 1));
+				} else {
+					frm.set_value("start_date", frappe.defaults.get_default("year_start_date"));
+				}
+			});
+	},
+
+	start_date: function (frm) {
+		frm.set_value(
+			"end_date",
+			frappe.datetime.add_days(frappe.datetime.add_months(frm.doc.start_date, 12), -1),
+		);
+>>>>>>> 22a9000b3 (fix: set payroll period dates based on last payroll period/current fiscal year)
 	},
 >>>>>>> a3060da3d (feat(Create Payroll Period): set Start & End Date as that of Fiscal Year if no Payroll Period exists)
 });
