6b4b6fb115c2081e408cd0c31867f2580986f903
Pankaj <frappeuser@ip-172-31-26-38.ap-south-1.compute.internal>
2024-05-07 04:24:51 +0000

    from live 7.5.24

--- hrms/hr/web_form/job_application/job_application.py
+++ hrms/hr/web_form/job_application/job_application.py
@@ -1,3 +1,9 @@
+# your_custom_app/your_custom_app/page/job_application/job_application.py
+
+import frappe
+from frappe import _
+
 def get_context(context):
-	# do your magic here
-	pass
+    pass
+
+
