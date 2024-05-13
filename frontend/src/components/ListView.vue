c3a72afa5762e88781bbe28ee7ec538ecf356df2
Viny Selopal <viny0698@gmail.com>
2024-05-13 14:02:14 +0530

    feat(list view sort): add sort button, modal and Sort Action functionalities to ListView

--- frontend/src/components/ListView.vue
+++ frontend/src/components/ListView.vue
@@ -1,31 +1,21 @@
 <template>
 	<ion-header class="ion-no-border">
 		<div class="w-full sm:w-96">
-			<div
-				class="flex flex-row bg-white shadow-sm py-4 px-3 items-center justify-between border-b"
-			>
+			<div class="flex flex-row bg-white shadow-sm py-4 px-3 items-center justify-between border-b">
 				<div class="flex flex-row items-center">
-					<Button
-						variant="ghost"
-						class="!pl-0 hover:bg-white"
-						@click="router.back()"
-					>
+					<Button variant="ghost" class="!pl-0 hover:bg-white" @click="router.back()">
 						<FeatherIcon name="chevron-left" class="h-5 w-5" />
 					</Button>
 					<h2 class="text-xl font-semibold text-gray-900">{{ pageTitle }}</h2>
 				</div>
 
 				<div class="flex flex-row gap-2">
-					<Button
-						id="show-filter-modal"
-						icon="filter"
-						variant="subtle"
-						:class="[
-							areFiltersApplied
-								? '!border !border-gray-800 !bg-white !text-gray-900 !font-semibold'
-								: '',
-						]"
-					/>
+					<Button id="show-filter-modal" icon="filter" variant="subtle" :class="[
+						areFiltersApplied
+							? '!border !border-gray-800 !bg-white !text-gray-900 !font-semibold'
+							: '',
+					]" />
+					<Button id="show-sort-modal" icon="sort" variant="subtle" :class="[]" />
 					<router-link :to="{ name: formViewRoute }" v-slot="{ navigate }">
 						<Button variant="solid" class="mr-2" @click="navigate">
 							<template #prefix>
@@ -44,50 +34,24 @@
 			<ion-refresher-content></ion-refresher-content>
 		</ion-refresher>
 
-		<div
-			class="flex flex-col items-center mb-7 p-4 h-full w-full sm:w-96 overflow-y-auto"
-			ref="scrollContainer"
-			@scroll="() => handleScroll()"
-		>
+		<div class="flex flex-col items-center mb-7 p-4 h-full w-full sm:w-96 overflow-y-auto" ref="scrollContainer"
+			@scroll="() => handleScroll()">
 			<div class="w-full mt-5">
-				<TabButtons
-					:buttons="[{ label: tabButtons[0] }, { label: tabButtons[1] }]"
-					v-model="activeTab"
-				/>
+				<TabButtons :buttons="[{ label: tabButtons[0] }, { label: tabButtons[1] }]" v-model="activeTab" />
 
-				<div
-					class="flex flex-col bg-white rounded mt-5"
-					v-if="!documents.loading && documents.data?.length"
-				>
-					<div
-						class="p-3.5 items-center justify-between border-b cursor-pointer"
-						v-for="link in documents.data"
-						:key="link.name"
-					>
-						<router-link
-							:to="{ name: detailViewRoute, params: { id: link.name } }"
-							v-slot="{ navigate }"
-						>
-							<component
-								:is="listItemComponent[doctype]"
-								:doc="link"
-								:isTeamRequest="isTeamRequest"
-								:workflowStateField="workflowStateField"
-								@click="navigate"
-							/>
+				<div class="flex flex-col bg-white rounded mt-5" v-if="!documents.loading && documents.data?.length">
+					<div class="p-3.5 items-center justify-between border-b cursor-pointer"
+						v-for="link in documents.data" :key="link.name">
+						<router-link :to="{ name: detailViewRoute, params: { id: link.name } }" v-slot="{ navigate }">
+							<component :is="listItemComponent[doctype]" :doc="link" :isTeamRequest="isTeamRequest"
+								:workflowStateField="workflowStateField" @click="navigate" />
 						</router-link>
 					</div>
 				</div>
-				<EmptyState
-					:message="`No ${props.doctype?.toLowerCase()}s found`"
-					v-else-if="!documents.loading"
-				/>
+				<EmptyState :message="`No ${props.doctype?.toLowerCase()}s found`" v-else-if="!documents.loading" />
 
 				<!-- Loading Indicator -->
-				<div
-					v-if="documents.loading"
-					class="flex mt-2 items-center justify-center"
-				>
+				<div v-if="documents.loading" class="flex mt-2 items-center justify-center">
 					<LoadingIndicator class="w-8 h-8 text-gray-800" />
 				</div>
 			</div>
@@ -96,12 +60,16 @@
 		<CustomIonModal trigger="show-filter-modal">
 			<!-- Filter Action Sheet -->
 			<template #actionSheet>
-				<ListFiltersActionSheet
-					:filterConfig="filterConfig"
-					@applyFilters="applyFilters"
-					@clearFilters="clearFilters"
-					v-model:filters="filterMap"
-				/>
+				<ListFiltersActionSheet :filterConfig="filterConfig" @applyFilters="applyFilters"
+					@clearFilters="clearFilters" v-model:filters="filterMap" />
+			</template>
+		</CustomIonModal>
+
+
+		<CustomIonModal trigger="show-sort-modal">
+			<!-- Sort Action Sheet -->
+			<template #actionSheet>
+				<ListSortActionSheet :sortFields="fields" @applySort="applySort" v-model:sortOrder="sortOrder" />
 			</template>
 		</CustomIonModal>
 	</ion-content>
@@ -130,6 +98,7 @@ import LeaveRequestItem from "@/components/LeaveRequestItem.vue"
 import ExpenseClaimItem from "@/components/ExpenseClaimItem.vue"
 import EmployeeAdvanceItem from "@/components/EmployeeAdvanceItem.vue"
 import ListFiltersActionSheet from "@/components/ListFiltersActionSheet.vue"
+import ListSortActionSheet from "@/components/ListSortActionSheet.vue"
 import CustomIonModal from "@/components/CustomIonModal.vue"
 
 import useWorkflow from "@/composables/workflow"
@@ -176,6 +145,10 @@ const activeTab = ref(props.tabButtons[0])
 const areFiltersApplied = ref(false)
 const appliedFilters = ref([])
 const workflowStateField = ref(null)
+const sortOrder = reactive({
+	field: "Modified",
+	order: "asc"
+})
 
 // infinite scroll
 const scrollContainer = ref(null)
@@ -184,9 +157,14 @@ const listOptions = ref({
 	doctype: props.doctype,
 	fields: props.fields,
 	group_by: props.groupBy,
-	order_by: `\`tab${props.doctype}\`.modified desc`,
+	order_by: `\`tab${props.doctype}\`.${sortOrder.field} ${sortOrder.order}`,
 	page_length: 50,
 })
+
+watch("sortOrder", (newValue, oldValue) => {
+	console.log(newValue);
+}
+)
 
 // computed properties
 const isTeamRequest = computed(() => {
@@ -291,6 +269,11 @@ function clearFilters() {
 	fetchDocumentList()
 	modalController.dismiss()
 	areFiltersApplied.value = false
+}
+
+function applySort() {
+	fetchDocumentList()
+	modalController.dismiss()
 }
 
 function fetchDocumentList(start = 0) {
