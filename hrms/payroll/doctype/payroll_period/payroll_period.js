eb99caabbcf005064448e18974076157371f7900
Rucha Mahabal <ruchamahabal2@gmail.com>
2024-05-14 16:45:41 +0530

    chore: remove redundant code for setting payroll period

--- hrms/payroll/doctype/payroll_period/payroll_period.js
+++ hrms/payroll/doctype/payroll_period/payroll_period.js
@@ -2,16 +2,6 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Payroll Period", {
-	refresh: function (frm) {
-		// sets start date and end date as that of the current fiscal year if no payroll period exists
-		frappe.db.exists("Payroll Period").then((period_exists) => {
-			if (!period_exists) {
-				frm.set_value("start_date", frappe.defaults.get_default("year_start_date"));
-				frm.set_value("end_date", frappe.defaults.get_default("year_end_date"));
-			}
-		});
-	},
-
 	onload: function (frm) {
 		frm.trigger("set_start_date");
 	},
