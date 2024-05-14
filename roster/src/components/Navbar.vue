fb0cf0f044d0483b5f456d7e5e2b2cf137890277
krantheman <akash.akasht.tom@gmail.com>
2024-05-14 14:35:00 +0530

    feat: add makeshift navbar
    (cherry picked from commit ac8cbc3c03a97d0238fb7f98a8e1878db0062c7d)

---
+++ roster/src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="h-12 bg-white border-b px-12 flex items-center">
+		<a class="text-xl" href="/">Home</a>
+		<Dropdown
+			class="ml-auto"
+			:options="[
+				{
+					label: 'My Account',
+				},
+				{
+					label: 'Log Out',
+				},
+				{
+					label: 'Switch to Desk',
+				},
+				,
+				,
+			]"
+		>
+			<Avatar size="lg" class="cursor-pointer" />
+		</Dropdown>
+	</div>
+</template>
+
+<script setup>
+import { Dropdown, Avatar } from "frappe-ui";
+</script>
