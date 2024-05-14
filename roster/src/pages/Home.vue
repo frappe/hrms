ac8cbc3c03a97d0238fb7f98a8e1878db0062c7d
krantheman <akash.akasht.tom@gmail.com>
2024-05-14 14:35:00 +0530

    feat: add makeshift navbar

--- roster/src/pages/Home.vue
+++ roster/src/pages/Home.vue
@@ -1,9 +1,13 @@
 <template>
-	<div class="p-12 bg-gray-50 h-screen">
-		<MonthView />
+	<div class="bg-gray-50 h-screen">
+		<Navbar />
+		<div class="p-12">
+			<MonthView />
+		</div>
 	</div>
 </template>
 
 <script setup>
+import Navbar from "../components/Navbar.vue";
 import MonthView from "../components/MonthView.vue";
 </script>
