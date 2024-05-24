f5bc95f92a19664b37b6994d40d555e5ddf846c5
krantheman <akash.akasht.tom@gmail.com>
2024-05-24 13:51:43 +0530

    feat: get_leaves api

--- hrms/api/roster.py
+++ hrms/api/roster.py
@@ -17,16 +17,76 @@ def get_events(
 	employee_filters: dict[str, str],
 	shift_filters: dict[str, str],
 ) -> dict[str, list[dict]]:
-	shifts = get_shifts(month_start, month_end, employee_filters, shift_filters)
 	holidays = get_holidays(month_start, month_end, employee_filters)
+	leaves = get_leaves(month_start, month_end, employee_filters)
+	shifts = get_shifts(month_start, month_end, employee_filters, shift_filters)
 
-	events = shifts.copy()
-	for key, value in holidays.items():
-		if key in events:
-			events[key].extend(value)
-		else:
-			events[key] = value
+	events = {}
+	for event in [holidays, leaves, shifts]:
+		event = group_by_employee(event)
+		for key, value in event.items():
+			if key in events:
+				events[key].extend(value)
+			else:
+				events[key] = value
 	return events
+
+
+def get_holidays(month_start: str, month_end: str, employee_filters: dict[str, str]) -> dict[str, list[dict]]:
+	Employee = frappe.qb.DocType("Employee")
+	HolidayList = frappe.qb.DocType("Holiday List")
+	Holiday = frappe.qb.DocType("Holiday")
+
+	query = (
+		frappe.qb.select(
+			Employee.employee,
+			Holiday.name.as_("holiday"),
+			Holiday.holiday_date,
+			Holiday.description,
+			Holiday.weekly_off,
+		)
+		.from_(Employee)
+		.join(HolidayList)
+		.on(Employee.holiday_list == HolidayList.name)
+		.join(Holiday)
+		.on(Holiday.parent == HolidayList.name)
+		.where(Holiday.holiday_date[month_start:month_end])
+	)
+
+	for filter in employee_filters:
+		query = query.where(Employee[filter] == employee_filters[filter])
+
+	return query.run(as_dict=True)
+
+
+def get_leaves(month_start: str, month_end: str, employee_filters: dict[str, str]) -> dict[str, list[dict]]:
+	LeaveApplication = frappe.qb.DocType("Leave Application")
+	Employee = frappe.qb.DocType("Employee")
+
+	query = (
+		frappe.qb.select(
+			LeaveApplication.name.as_("leave"),
+			LeaveApplication.employee,
+			LeaveApplication.leave_type,
+			LeaveApplication.from_date,
+			LeaveApplication.to_date,
+			LeaveApplication.half_day,
+		)
+		.from_(LeaveApplication)
+		.left_join(Employee)
+		.on(LeaveApplication.employee == Employee.name)
+		.where(
+			(LeaveApplication.docstatus == 1)
+			& (LeaveApplication.status == "Approved")
+			& (LeaveApplication.from_date <= month_end)
+			& (LeaveApplication.to_date >= month_start)
+		)
+	)
+
+	for filter in employee_filters:
+		query = query.where(Employee[filter] == employee_filters[filter])
+
+	return query.run(as_dict=True)
 
 
 def get_shifts(
@@ -66,47 +126,13 @@ def get_shifts(
 	for filter in shift_filters:
 		query = query.where(ShiftAssignment[filter] == shift_filters[filter])
 
-	shifts = query.run(as_dict=True)
+	return query.run(as_dict=True)
 
-	# group shifts under employee by converting list to dict
-	grouped_shifts = {}
-	for shift in shifts:
-		grouped_shifts.setdefault(shift["employee"], []).append(
-			{k: v for k, v in shift.items() if k != "employee"}
+
+def group_by_employee(events: list[dict]) -> dict[str, list[dict]]:
+	grouped_events = {}
+	for event in events:
+		grouped_events.setdefault(event["employee"], []).append(
+			{k: v for k, v in event.items() if k != "employee"}
 		)
-	return grouped_shifts
-
-
-def get_holidays(month_start: str, month_end: str, employee_filters: dict[str, str]) -> dict[str, list[dict]]:
-	Employee = frappe.qb.DocType("Employee")
-	HolidayList = frappe.qb.DocType("Holiday List")
-	Holiday = frappe.qb.DocType("Holiday")
-
-	query = (
-		frappe.qb.select(
-			Employee.employee,
-			Holiday.name,
-			Holiday.holiday_date,
-			Holiday.description,
-			Holiday.weekly_off,
-		)
-		.from_(Employee)
-		.join(HolidayList)
-		.on(Employee.holiday_list == HolidayList.name)
-		.join(Holiday)
-		.on(Holiday.parent == HolidayList.name)
-		.where(Holiday.holiday_date[month_start:month_end])
-	)
-
-	for filter in employee_filters:
-		query = query.where(Employee[filter] == employee_filters[filter])
-
-	holidays = query.run(as_dict=True)
-
-	# group holidays under employee by converting list to dict
-	grouped_holidays = {}
-	for holiday in holidays:
-		grouped_holidays.setdefault(holiday["employee"], []).append(
-			{k: v for k, v in holiday.items() if k != "employee"}
-		)
-	return grouped_holidays
+	return grouped_events
