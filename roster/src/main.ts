3df285a6ec96a84aec951fa1468e30df3ae1b8f1
krantheman <akash.akasht.tom@gmail.com>
2024-05-30 15:50:57 +0530

    revert: removal of vue-router

--- roster/src/main.ts
+++ roster/src/main.ts
@@ -1,6 +1,7 @@
 import "./index.css";
 
 import { createApp } from "vue";
+import router from "./router";
 import App from "./App.vue";
 
 import { Button, setConfig, frappeRequest, resourcesPlugin } from "frappe-ui";
@@ -9,6 +10,7 @@ const app = createApp(App);
 
 setConfig("resourceFetcher", frappeRequest);
 
+app.use(router);
 app.use(resourcesPlugin);
 
 app.component("Button", Button);
---
+++ roster/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="bg-gray-50 min-h-screen">
+		<NavBar />
+		<MonthView />
+		<Toasts />
+	</div>
+</template>
+
+<script setup lang="ts">
+import { Toasts } from "frappe-ui";
+import NavBar from "../components/NavBar.vue";
+import MonthView from "./MonthView.vue";
+</script>
