57582b210d9d22d9b417ac6e816f89a3f8cb33b4
krantheman <akash.akasht.tom@gmail.com>
2024-05-30 15:14:26 +0530

    chore: remove vue router
    (cherry picked from commit 4262c1b71b79eb489effe924c1b4256197fb12cd)

--- roster/src/App.vue
+++ roster/src/App.vue
@@ -1,5 +1,14 @@
 <template>
-	<div>
-		<router-view />
+	<div class="bg-gray-50 min-h-screen">
+		<NavBar />
+		<MonthView />
+		<Toasts />
 	</div>
 </template>
+
+<script setup lang="ts">
+import { Toasts } from "frappe-ui";
+
+import NavBar from "./components/NavBar.vue";
+import MonthView from "./views/MonthView.vue";
+</script>
--- roster/src/main.ts
+++ roster/src/main.ts
@@ -1,7 +1,6 @@
 import "./index.css";
 
 import { createApp } from "vue";
-import router from "./router";
 import App from "./App.vue";
 
 import { Button, setConfig, frappeRequest, resourcesPlugin } from "frappe-ui";
@@ -10,7 +9,6 @@ const app = createApp(App);
 
 setConfig("resourceFetcher", frappeRequest);
 
-app.use(router);
 app.use(resourcesPlugin);
 
 app.component("Button", Button);
