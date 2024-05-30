5e015d5b0e8a8eccf0c92709999348d7c451a684
krantheman <akash.akasht.tom@gmail.com>
2024-05-30 13:41:01 +0530

    refactor: move employees resource to MonthView

--- roster/src/pages/MonthView.vue
+++ roster/src/pages/MonthView.vue
@@ -1,38 +1,97 @@
 <template>
 	<div class="px-12 py-6 space-y-6">
+		<div class="flex items-center">
+			<FeatherIcon name="calendar" class="h-7 w-7 text-gray-500 mr-3" />
+			<span class="font-semibold text-2xl mr-1">Month View</span>
+			<Dropdown
+				:options="[
+					{
+						label: 'Shift Assignment',
+						onClick: () => {
+							showShiftAssignmentDialog = true;
+						},
+					},
+				]"
+				:button="{
+					label: 'Create',
+					variant: 'solid',
+					iconRight: 'chevron-down',
+					size: 'md',
+				}"
+				class="ml-auto"
+			/>
+		</div>
 		<div class="bg-white rounded-lg border p-4">
 			<MonthViewHeader
 				:firstOfMonth="firstOfMonth"
 				@updateFilters="updateFilters"
 				@addToMonth="addToMonth"
 			/>
-			<MonthViewTable :firstOfMonth="firstOfMonth" :filters="filters" />
+			<MonthViewTable
+				:firstOfMonth="firstOfMonth"
+				:employees="employees.data"
+				:employeeFilters="employeeFilters"
+				:shiftTypeFilter="shiftTypeFilter"
+			/>
 		</div>
 	</div>
+	<ShiftAssignmentDialog
+		v-model="showShiftAssignmentDialog"
+		:isDialogOpen="showShiftAssignmentDialog"
+		:employees="employees.data"
+	/>
 </template>
 
 <script setup lang="ts">
 import { ref, reactive } from "vue";
+import { Dropdown, FeatherIcon, createListResource } from "frappe-ui";
 
-import { dayjs } from "../utils";
+import { dayjs, raiseToast } from "../utils";
 import MonthViewTable from "../components/MonthViewTable.vue";
 import MonthViewHeader from "../components/MonthViewHeader.vue";
+import ShiftAssignmentDialog from "../components/ShiftAssignmentDialog.vue";
 
+export type EmployeeFilters = {
+	[K in "status" | "company" | "department" | "branch" | "designation"]?: string;
+};
+
+const showShiftAssignmentDialog = ref(false);
 const firstOfMonth = ref(dayjs().date(1).startOf("D"));
-
-const filters = reactive({
-	company: "",
-	department: "",
-	branch: "",
-	designation: "",
-	shift_type: "",
+const shiftTypeFilter = ref("");
+const employeeFilters = reactive<EmployeeFilters>({
+	status: "Active",
 });
 
 const addToMonth = (change: number) => {
 	firstOfMonth.value = firstOfMonth.value.add(change, "M");
 };
 
-const updateFilters = (newFilters: typeof filters) => {
-	Object.assign(filters, newFilters);
+const updateFilters = (newFilters: EmployeeFilters & { shift_type: string }) => {
+	let employeeUpdated = false;
+	(Object.entries(newFilters) as [keyof EmployeeFilters | "shift_type", string][]).forEach(
+		([key, value]) => {
+			if (key === "shift_type") {
+				shiftTypeFilter.value = value;
+				return;
+			}
+
+			if (value) employeeFilters[key] = value;
+			else delete employeeFilters[key];
+			employeeUpdated = true;
+		},
+	);
+	if (employeeUpdated) employees.fetch();
 };
+
+// RESOURCES
+
+const employees = createListResource({
+	doctype: "Employee",
+	fields: ["name", "employee_name", "designation", "image"],
+	filters: employeeFilters,
+	auto: true,
+	onError(error: { messages: string[] }) {
+		raiseToast("error", error.messages[0]);
+	},
+});
 </script>
