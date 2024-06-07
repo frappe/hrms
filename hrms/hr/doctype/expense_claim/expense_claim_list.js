50abee4ce49ac437838229e18327136df1bdfb9f
Rucha Mahabal <ruchamahabal2@gmail.com>
2024-06-07 16:29:00 +0530

    chore: fix conflicts

--- hrms/hr/doctype/expense_claim/expense_claim_list.js
+++ hrms/hr/doctype/expense_claim/expense_claim_list.js
@@ -2,7 +2,7 @@
 // License: GNU General Public License v3. See license.txt
 
 frappe.listview_settings["Expense Claim"] = {
-  add_fields: ["company"],
+	add_fields: ["company"],
 	onload(listview) {
 		listview.page.add_inner_button(__("Expense Claim Type"), function () {
 			frappe.set_route("List", "Expense Claim Type", {"name": listview.get_filter_value("expense_type")});
