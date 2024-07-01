93b82384bd5ce1b883810afe31ef8966ed1014f4
Rucha Mahabal <ruchamahabal2@gmail.com>
2024-07-01 11:35:59 +0530

    feat: populate withholding cycles in child table and set To Date
    - remove unnecessary fields

--- hrms/hr/doctype/salary_withholding/salary_withholding.py
+++ hrms/hr/doctype/salary_withholding/salary_withholding.py
@@ -1,9 +1,73 @@
 # Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+from dateutil.relativedelta import relativedelta
+
+import frappe
+from frappe import _
 from frappe.model.document import Document
+from frappe.utils import add_days, add_to_date, cint, getdate
 
 
 class SalaryWithholding(Document):
-	pass
+	@frappe.whitelist()
+	def set_withholding_cycles_and_to_date(self):
+		self.to_date = self.get_to_date()
+
+		cycle_from_date = cycle_to_date = getdate(self.from_date)
+		self.cycles = []
+
+		while cycle_to_date < getdate(self.to_date):
+			cycle_to_date = add_to_date(cycle_from_date, **self.get_frequency_kwargs()) - relativedelta(
+				days=1
+			)
+			self.append(
+				"cycles",
+				{
+					"from_date": cycle_from_date,
+					"to_date": cycle_to_date,
+					"is_salary_released": 0,
+				},
+			)
+			cycle_from_date = add_days(cycle_to_date, 1)
+
+	def get_to_date(self) -> str:
+		from_date = getdate(self.from_date)
+		kwargs = self.get_frequency_kwargs(self.number_of_withholding_cycles)
+		to_date = add_to_date(from_date, **kwargs) - relativedelta(days=1)
+		return to_date
+
+	def get_frequency_kwargs(self, withholding_cycles: int = 0) -> dict:
+		cycles = cint(withholding_cycles) or 1
+		frequency_dict = {
+			"Monthly": {"months": 1 * cycles},
+			"Bimonthly": {"months": 2 * cycles},
+			"Fortnightly": {"days": 14 * cycles},
+			"Weekly": {"days": 7 * cycles},
+			"Daily": {"days": 1 * cycles},
+		}
+		return frequency_dict.get(self.payroll_frequency)
+
+
+@frappe.whitelist()
+def get_payroll_frequency(employee: str, posting_date: str) -> str | None:
+	salary_structure = frappe.db.get_value(
+		"Salary Structure Assignment",
+		{
+			"employee": employee,
+			"from_date": ("<=", posting_date),
+			"docstatus": 1,
+		},
+		"salary_structure",
+		order_by="from_date desc",
+	)
+
+	if not salary_structure:
+		frappe.throw(
+			_("No Salary Structure Assignment found for employee {0} on or before {1}").format(
+				employee, posting_date
+			),
+			title=_("Error"),
+		)
+
+	return frappe.db.get_value("Salary Structure", salary_structure, "payroll_frequency")
