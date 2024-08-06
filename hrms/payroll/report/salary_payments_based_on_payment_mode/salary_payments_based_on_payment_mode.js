bf54c2861112a3a9a19b61e09d8a8d076b4a1093
Nihantra C. Patel <141945075+Nihantra-Patel@users.noreply.github.com>
2024-08-06 14:16:32 +0530

    fix: filter and bold issue in Salary Payments Based On Payment Mode (#2025)
    * fix: filter and bold issue in Salary Payments Based On Payment Mode
    
    * fix: filter and bold issue in Salary Payments Based On Payment Mode --prettier
    
    (cherry picked from commit 567ab9efccb8c5fd2a86525bcd7f2cf2cae181ee)

--- hrms/payroll/report/salary_payments_based_on_payment_mode/salary_payments_based_on_payment_mode.js
+++ hrms/payroll/report/salary_payments_based_on_payment_mode/salary_payments_based_on_payment_mode.js
@@ -2,7 +2,16 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
-frappe.require("assets/hrms/js/salary_slip_deductions_report_filters.js", function () {
-	frappe.query_reports["Salary Payments Based On Payment Mode"] =
-		hrms.salary_slip_deductions_report_filters;
-});
+frappe.query_reports["Salary Payments Based On Payment Mode"] = $.extend(
+	{},
+	hrms.salary_slip_deductions_report_filters,
+	{
+		formatter: function (value, row, column, data, default_formatter) {
+			value = default_formatter(value, row, column, data);
+			if (data.branch && data.branch.includes("Total") && column.colIndex === 1) {
+				value = value.bold();
+			}
+			return value;
+		},
+	},
+);
