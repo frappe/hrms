9a66efe1877a4b7f81678b8dc4ebdb0c780abcae
Rucha Mahabal <ruchamahabal2@gmail.com>
2024-08-06 13:20:23 +0530

    chore: fix conflicts

--- hrms/public/js/hrms.bundle.js
+++ hrms/public/js/hrms.bundle.js
@@ -1,8 +1,3 @@
 import "./templates/employees_with_unmarked_attendance.html";
 import "./utils";
-<<<<<<< HEAD
-=======
-import "./utils/payroll_utils";
-import "./utils/leave_utils";
 import "./salary_slip_deductions_report_filters.js";
->>>>>>> 559ef067a (fix: filter in reports (#1987))
