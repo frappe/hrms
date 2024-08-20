eb5a21ea350ea3162d4a5f262c5f8aaa590da773
krantheman <akash.akasht.tom@gmail.com>
2024-08-20 17:01:50 +0530

    feat: add calendar days

--- frontend/src/components/AttendanceCalendar.vue
+++ frontend/src/components/AttendanceCalendar.vue
@@ -1,24 +1,49 @@
 <template>
 	<div class="flex flex-col w-full gap-5" v-if="calendarEvents.data">
 		<div class="flex flex-row justify-between items-center px-4">
-			<Button
-				icon="chevron-left"
-				variant="ghost"
-				@click="firstOfMonth = firstOfMonth.subtract(1, 'M')"
-			/>
+			<div class="text-lg text-gray-800 font-bold">Attendance Calendar</div>
+		</div>
 
-			<span class="text-lg text-gray-800 font-bold">
-				{{ firstOfMonth.format("MMMM") }} {{ firstOfMonth.format("YYYY") }}
-			</span>
-			<Button
-				icon="chevron-right"
-				variant="ghost"
-				@click="firstOfMonth = firstOfMonth.add(1, 'M')"
-			/>
+		<div class="flex flex-col gap-4 bg-white py-6 px-3.5 rounded-lg border-none">
+			<div class="flex flex-row justify-between items-center px-4 mb-2">
+				<Button
+					icon="chevron-left"
+					variant="ghost"
+					@click="firstOfMonth = firstOfMonth.subtract(1, 'M')"
+				/>
+				<span class="text-lg text-gray-800 font-bold">
+					{{ firstOfMonth.format("MMMM") }} {{ firstOfMonth.format("YYYY") }}
+				</span>
+				<Button
+					icon="chevron-right"
+					variant="ghost"
+					@click="firstOfMonth = firstOfMonth.add(1, 'M')"
+				/>
+			</div>
+
+			<div class="grid grid-cols-7 gap-y-3">
+				<div
+					v-for="day in DAYS"
+					class="flex justify-center text-gray-600 text-sm font-medium leading-6"
+				>
+					{{ day }}
+				</div>
+				<div v-for="_ in firstOfMonth.get('d')" />
+				<div v-for="index in firstOfMonth.endOf('M').get('D')" class="flex justify-center">
+					<div
+						class="h-8 w-8 flex justify-center rounded-full"
+						:class="getEventOnDate(index) && `bg-${colorMap[getEventOnDate(index)]}-200`"
+					>
+						<span class="text-gray-800 text-sm font-medium leading-6 my-auto">
+							{{ index }}
+						</span>
+					</div>
+				</div>
+			</div>
 		</div>
 
 		<div class="flex flex-col gap-4 bg-white py-3 px-3.5 rounded-lg border-none">
-			<div class="grid grid-cols-3 gap-y-3">
+			<div class="grid grid-cols-3 gap-3">
 				<div v-for="[status, color] of Object.entries(colorMap)" class="flex flex-col gap-1">
 					<div class="flex flex-row gap-1 items-center">
 						<span class="text-gray-600 text-sm font-medium leading-5"> {{ status }} </span>
@@ -68,4 +93,10 @@ watch(
 		calendarEvents.fetch()
 	}
 )
+
+const getEventOnDate = (date) => {
+	return calendarEvents.data[firstOfMonth.value.date(date).format("YYYY-MM-DD")]
+}
+
+const DAYS = ["S", "M", "T", "W", "T", "F", "S"]
 </script>
