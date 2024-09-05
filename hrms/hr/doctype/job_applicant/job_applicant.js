f6d43a9e4c377ecb1ae6428a8f9eb6454a5f0d4c
NandhiniDevi <95607404+Nandhinidevi123@users.noreply.github.com>
2024-09-05 11:59:13 +0530

    fix: interview round make mandatory for create interview dialog

--- hrms/hr/doctype/job_applicant/job_applicant.js
+++ hrms/hr/doctype/job_applicant/job_applicant.js
@@ -90,6 +90,7 @@ frappe.ui.form.on("Job Applicant", {
 					fieldname: "interview_round",
 					fieldtype: "Link",
 					options: "Interview Round",
+					reqd:1,
 				},
 			],
 			primary_action_label: __("Create Interview"),
