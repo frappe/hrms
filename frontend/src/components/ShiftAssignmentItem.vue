6ce1acfbfaee98039d76dfdb2d099dd82d6c7a43
krantheman <akash.akasht.tom@gmail.com>
2024-09-18 18:40:59 +0530

    refactor: use ListItem component for all items

--- frontend/src/components/ShiftAssignmentItem.vue
+++ frontend/src/components/ShiftAssignmentItem.vue
@@ -1,37 +1,35 @@
 <template>
-	<div class="flex flex-col w-full justify-center gap-2.5">
-		<div class="flex flex-row items-center justify-between">
-			<div class="flex flex-row items-start gap-3 grow">
-				<ShiftIcon class="h-5 w-5 text-gray-500" />
-				<div class="flex flex-col items-start gap-1.5">
-					<div class="text-base font-normal text-gray-800">
-						{{ props.doc.shift_type }}
-					</div>
-					<div class="text-xs font-normal text-gray-500">
-						<span>{{ props.doc.shift_dates || getShiftDates(props.doc) }}</span>
-						<span v-if="props.doc.end_date" class="whitespace-pre"> &middot; </span>
-						<span v-if="props.doc.end_date" class="whitespace-nowrap">{{
-							`${props.doc.total_shift_days || getTotalShiftDays(props.doc)}d`
-						}}</span>
-					</div>
+	<ListItem>
+		<template #left>
+			<ShiftIcon class="h-5 w-5 text-gray-500" />
+			<div class="flex flex-col items-start gap-1.5">
+				<div class="text-base font-normal text-gray-800">
+					{{ props.doc.shift_type }}
+				</div>
+				<div class="text-xs font-normal text-gray-500">
+					<span>{{ props.doc.shift_dates || getShiftDates(props.doc) }}</span>
+					<span v-if="props.doc.end_date" class="whitespace-pre"> &middot; </span>
+					<span v-if="props.doc.end_date" class="whitespace-nowrap">{{
+						`${props.doc.total_shift_days || getTotalShiftDays(props.doc)}d`
+					}}</span>
 				</div>
 			</div>
-			<div class="flex flex-row justify-end items-center gap-2">
-				<span v-if="props.doc.shift_timing" class="text-gray-700 font-normal rounded text-base">
-					{{ props.doc.shift_timing }}
-				</span>
-				<Badge v-else variant="outline" :theme="colorMap[status]" :label="status" size="md" />
-				<FeatherIcon name="chevron-right" class="h-5 w-5 text-gray-500" />
-			</div>
-		</div>
-	</div>
+		</template>
+		<template #right>
+			<span v-if="props.doc.shift_timing" class="text-gray-700 font-normal rounded text-base">
+				{{ props.doc.shift_timing }}
+			</span>
+			<Badge v-else variant="outline" :theme="colorMap[status]" :label="status" size="md" />
+			<FeatherIcon name="chevron-right" class="h-5 w-5 text-gray-500" />
+		</template>
+	</ListItem>
 </template>
 
 <script setup>
 import { computed } from "vue"
 import { Badge, FeatherIcon } from "frappe-ui"
 
-import EmployeeAvatar from "@/components/EmployeeAvatar.vue"
+import ListItem from "@/components/ListItem.vue"
 import ShiftIcon from "@/components/icons/ShiftIcon.vue"
 import { getShiftDates, getTotalShiftDays } from "@/data/attendance"
 
