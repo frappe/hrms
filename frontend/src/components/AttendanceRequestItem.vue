4a35edda69964b2f0f2bd18fbd2dc468df127dc9
krantheman <akash.akasht.tom@gmail.com>
2024-09-18 18:40:59 +0530

    refactor: use ListItem component for all items

--- frontend/src/components/AttendanceRequestItem.vue
+++ frontend/src/components/AttendanceRequestItem.vue
@@ -1,36 +1,34 @@
 <template>
-	<div class="flex flex-col w-full justify-center gap-2.5">
-		<div class="flex flex-row items-center justify-between">
-			<div class="flex flex-row items-start gap-3 grow">
-				<AttendanceIcon class="h-5 w-5 text-gray-500" />
-				<div class="flex flex-col items-start gap-1.5">
-					<div class="text-base font-normal text-gray-800">
-						{{ props.doc.reason }}
-					</div>
-					<div class="text-xs font-normal text-gray-500">
-						<span>{{ props.doc.attendance_dates || getDates(props.doc) }}</span>
-						<span v-if="props.doc.to_date">
-							<span class="whitespace-pre"> &middot; </span>
-							<span class="whitespace-nowrap">{{
-								`${props.doc.total_attendance_days || getTotalDays(props.doc)}d`
-							}}</span>
-						</span>
-					</div>
+	<ListItem>
+		<template #left>
+			<AttendanceIcon class="h-5 w-5 text-gray-500" />
+			<div class="flex flex-col items-start gap-1.5">
+				<div class="text-base font-normal text-gray-800">
+					{{ props.doc.reason }}
+				</div>
+				<div class="text-xs font-normal text-gray-500">
+					<span>{{ props.doc.attendance_dates || getDates(props.doc) }}</span>
+					<span v-if="props.doc.to_date">
+						<span class="whitespace-pre"> &middot; </span>
+						<span class="whitespace-nowrap">{{
+							`${props.doc.total_attendance_days || getTotalDays(props.doc)}d`
+						}}</span>
+					</span>
 				</div>
 			</div>
-			<div class="flex flex-row justify-end items-center gap-2">
-				<Badge variant="outline" :theme="colorMap[status]" :label="status" size="md" />
-				<FeatherIcon name="chevron-right" class="h-5 w-5 text-gray-500" />
-			</div>
-		</div>
-	</div>
+		</template>
+		<template #right>
+			<Badge variant="outline" :theme="colorMap[status]" :label="status" size="md" />
+			<FeatherIcon name="chevron-right" class="h-5 w-5 text-gray-500" />
+		</template>
+	</ListItem>
 </template>
 
 <script setup>
 import { computed } from "vue"
 import { Badge, FeatherIcon } from "frappe-ui"
 
-import EmployeeAvatar from "@/components/EmployeeAvatar.vue"
+import ListItem from "@/components/ListItem.vue"
 import AttendanceIcon from "@/components/icons/AttendanceIcon.vue"
 import { getDates, getTotalDays } from "@/data/attendance"
 
---
+++ frontend/src/components/ListItem.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="flex flex-col w-full justify-center gap-2.5">
+		<div class="flex flex-row items-center justify-between">
+			<div class="flex flex-row items-start gap-3 grow">
+				<slot name="left" />
+			</div>
+			<div class="flex flex-row justify-end items-center gap-2">
+				<slot name="right" />
+			</div>
+		</div>
+		<div v-if="props.isTeamRequest" class="flex flex-row items-center gap-2 pl-8">
+			<EmployeeAvatar :employeeID="props.employee" />
+			<div class="text-sm text-gray-600 grow">
+				{{ props.employeeName }}
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import EmployeeAvatar from "@/components/EmployeeAvatar.vue"
+
+const props = defineProps({
+	isTeamRequest: {
+		type: Boolean,
+		default: false,
+	},
+	employee: {
+		type: String,
+		required: false,
+	},
+	employeeName: {
+		type: String,
+		required: false,
+	},
+})
+</script>
