e5128afad83435a06a8a6d6dae2d1199f3e02eaf
David <dgx.arnold@gmail.com>
2024-10-02 01:03:10 +0200

    ci: make it preemtible with pending upstream PRs

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -10,7 +10,11 @@ sudo apt install libcups2-dev redis-server mariadb-client-10.6
 
 pip install frappe-bench
 
-git clone https://github.com/frappe/frappe --branch "$BRANCH_TO_CLONE" --depth 1
+githubbranch=${GITHUB_BASE_REF:-${GITHUB_REF##*/}}
+frappeuser=${FRAPPE_USER:-"frappe"}
+frappebranch=${FRAPPE_BRANCH:-$githubbranch}
+
+git clone "https://github.com/${frappeuser}/frappe" --branch "${frappebranch}" --depth 1
 bench init --skip-assets --frappe-path ~/frappe --python "$(which python)" frappe-bench
 
 mkdir ~/frappe-bench/sites/test_site
@@ -40,9 +44,9 @@ sed -i 's/schedule:/# schedule:/g' Procfile
 sed -i 's/socketio:/# socketio:/g' Procfile
 sed -i 's/redis_socketio:/# redis_socketio:/g' Procfile
 
-bench get-app payments --branch ${BRANCH_TO_CLONE%"-hotfix"}
-bench get-app https://github.com/frappe/erpnext --branch "$BRANCH_TO_CLONE" --resolve-deps
-bench get-app https://github.com/frappe/lending --branch ${BRANCH_TO_CLONE%"-hotfix"}
+bench get-app payments --branch ${githubbranch%"-hotfix"}
+bench get-app https://github.com/frappe/erpnext --branch "$githubbranch" --resolve-deps
+bench get-app https://github.com/frappe/lending --branch ${githubbranch%"-hotfix"}
 bench get-app hrms "${GITHUB_WORKSPACE}"
 bench setup requirements --dev
 
