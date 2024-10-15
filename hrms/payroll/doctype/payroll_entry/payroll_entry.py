9566fd3f41713455d6b3422140f179204747badb
Nihantra Patel <nihantra@frappe.io>
2024-10-15 12:01:02 +0530

    fix: payroll bank entry with the Loan Repayment amount
    (cherry picked from commit 44ffdb0c91294c60d170d36d5bafac568c6828f7)
    
    # Conflicts:
    #	hrms/payroll/doctype/payroll_entry/payroll_entry.py

--- hrms/payroll/doctype/payroll_entry/payroll_entry.py
+++ hrms/payroll/doctype/payroll_entry/payroll_entry.py
@@ -717,12 +717,104 @@ class PayrollEntry(Document):
 			"Payroll Settings", "process_payroll_accounting_entry_based_on_employee"
 		)
 
+<<<<<<< HEAD
 		salary_slip_name_list = frappe.db.sql(
 			""" select t1.name from `tabSalary Slip` t1
 			where t1.docstatus = 1 and start_date >= %s and end_date <= %s and t1.payroll_entry = %s
 			""",
 			(self.start_date, self.end_date, self.name),
 			as_list=True,
+=======
+		salary_slip_total = 0
+		salary_slips = self.get_salary_slip_details(for_withheld_salaries)
+
+		for salary_detail in salary_slips:
+			if salary_detail.parentfield == "earnings":
+				(
+					is_flexible_benefit,
+					only_tax_impact,
+					create_separate_je,
+					statistical_component,
+				) = frappe.db.get_value(
+					"Salary Component",
+					salary_detail.salary_component,
+					(
+						"is_flexible_benefit",
+						"only_tax_impact",
+						"create_separate_payment_entry_against_benefit_claim",
+						"statistical_component",
+					),
+					cache=True,
+				)
+
+				if only_tax_impact != 1 and statistical_component != 1:
+					if is_flexible_benefit == 1 and create_separate_je == 1:
+						self.set_accounting_entries_for_bank_entry(
+							salary_detail.amount, salary_detail.salary_component
+						)
+					else:
+						if employee_wise_accounting_enabled:
+							self.set_employee_based_payroll_payable_entries(
+								"earnings",
+								salary_detail.employee,
+								salary_detail.amount,
+								salary_detail.salary_structure,
+							)
+						salary_slip_total += salary_detail.amount
+
+			if salary_detail.parentfield == "deductions":
+				statistical_component = frappe.db.get_value(
+					"Salary Component", salary_detail.salary_component, "statistical_component", cache=True
+				)
+
+				if not statistical_component:
+					if employee_wise_accounting_enabled:
+						self.set_employee_based_payroll_payable_entries(
+							"deductions",
+							salary_detail.employee,
+							salary_detail.amount,
+							salary_detail.salary_structure,
+						)
+
+					salary_slip_total -= salary_detail.amount
+
+			salary_slip_total -= flt(salary_detail.get("total_loan_repayment"))
+
+		bank_entry = None
+		if salary_slip_total > 0:
+			remark = "withheld salaries" if for_withheld_salaries else "salaries"
+			bank_entry = self.set_accounting_entries_for_bank_entry(salary_slip_total, remark)
+
+			if for_withheld_salaries:
+				link_bank_entry_in_salary_withholdings(salary_slips, bank_entry.name)
+
+		return bank_entry
+
+	def get_salary_slip_details(self, for_withheld_salaries=False):
+		SalarySlip = frappe.qb.DocType("Salary Slip")
+		SalaryDetail = frappe.qb.DocType("Salary Detail")
+
+		query = (
+			frappe.qb.from_(SalarySlip)
+			.join(SalaryDetail)
+			.on(SalarySlip.name == SalaryDetail.parent)
+			.select(
+				SalarySlip.name,
+				SalarySlip.employee,
+				SalarySlip.salary_structure,
+				SalarySlip.salary_withholding_cycle,
+				SalarySlip.total_loan_repayment,
+				SalaryDetail.salary_component,
+				SalaryDetail.amount,
+				SalaryDetail.parentfield,
+			)
+			.where(
+				(SalarySlip.docstatus == 1)
+				& (SalarySlip.start_date >= self.start_date)
+				& (SalarySlip.end_date <= self.end_date)
+				& (SalarySlip.payroll_entry == self.name)
+			)
+>>>>>>> 44ffdb0c9 (fix: payroll bank entry with the Loan Repayment amount)
 		)
 
 		if salary_slip_name_list and len(salary_slip_name_list) > 0:
