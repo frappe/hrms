6e4596d6aab896903a300e552a64f13ce4a05867
Terence Adrien Zama <terence.zama@gmail.com>
2025-02-20 07:35:10 +0400

    hr changes

--- hrms/hr/doctype/provisional_plan/provisional_plan.py
+++ hrms/hr/doctype/provisional_plan/provisional_plan.py
@@ -18,6 +18,9 @@ class ProvisionalPlan(Document):
 
 @frappe.whitelist()
 def create_shifts(provisional_plan):
+	# frappe.cancel_doc("Shift Assignment", {"provisional_plan": provisional_plan})
+	frappe.db.sql(f"DELETE FROM `tabShift Assignment` WHERE provisional_plan = '{provisional_plan}'")
+
 	doc = frappe.get_doc("Provisional Plan", provisional_plan)
 	employees = doc.employees
 	shifts = json.loads(doc.shifts)
@@ -31,7 +34,19 @@ def create_shifts(provisional_plan):
 	num_days = calendar.monthrange(start_date.year, start_date.month)[1]
 
 	result_shifts = []
+	shift_types = frappe.get_all("Shift Type", filters=[
+		["department", "in", [department.name, department.parent_department]],
+	], fields=["name", "shift_suffix", "start_time", "end_time"])
+	shift_map = {}
+	for shift_type in shift_types:
+		shift_map[shift_type.shift_suffix] = shift_type
+
+	
 	for ei, employee in enumerate(employees):
+		emp = frappe.get_doc("Employee", employee.employee)
+		emp_shifts = []
+
+		# generate shifts for the month
 		for i in range(num_days):
 			shift_index = (i + 1 - (start_date_index - (ei * 7))) % len(flat_shifts)
 			s = ""
@@ -39,25 +54,32 @@ def create_shifts(provisional_plan):
 				s = flat_shifts[shift_index]
 			else:
 				s = flat_shifts[len(flat_shifts) + shift_index]
-
+				
+			s_date = (first_date_of_month + datetime.timedelta(days=i)).strftime("%Y-%m-%d")
+			
 			if s != "R":
+				shift_assignment = frappe.new_doc("Shift Assignment")
+				shift_assignment.employee = emp.employee
+				shift_assignment.start_date = s_date
+				shift_assignment.end_date = s_date
+				shift_assignment.shift_type = shift_map[s].name
+				shift_assignment.department = department.name
+				shift_assignment.company = company
+				# shift_assignment.docstatus = 1
+				shift_assignment.status = "Active"
+				shift_assignment.provisional_plan = doc.name
+				shift_assignment.submit()
 
-				shift_assignment = [
-					f"R-{employee.employee}-{(first_date_of_month + datetime.timedelta(days=i)).strftime('%Y-%m-%d')}-{department.name}",
-					 employee.employee,
-					 (first_date_of_month + datetime.timedelta(days=i)).strftime("%Y-%m-%d"),
-					 s,
-					 department.name,
-					 company,
-					 True,
-					 "Active"
-				]
-				result_shifts.append(shift_assignment)
 
 
-	frappe.db.bulk_insert("Shift Assignment",
-					    fields=["name","employee","start_date","shift_type","department","company","docstatus","status"],
-						  values=result_shifts)
+	
+
+	frappe.db.commit()
+	# frappe.db.bulk_insert("Shift Assignment",
+	# 				    fields=["name","employee","start_date","shift_type","department","company","docstatus","status", "provisional_plan"],
+	# 					  values=result_shifts)
+	# for s in result_shifts:
+	# 	print(s)
 
 	# frappe.db.set_value('Provisional Plan', provisional_plan, 'real_shift_assigned', 1)
 
--- hrms/public/js/roster/components/MonthlyReal.vue
+++ hrms/public/js/roster/components/MonthlyReal.vue
@@ -1,293 +1,377 @@
 <script setup lang="ts">
-import { ref, computed, defineProps } from 'vue'
-import moment from 'moment'
+import { ref, computed, defineProps, onMounted } from "vue";
+import moment from "moment";
 
-
-
-
-
-
-const props = defineProps(['shifts', 'employees', 'startDate', 'reals'])
+const props = defineProps([
+  "shifts",
+  "employees",
+  "startDate",
+  "reals",
+  "department",
+]);
 const startDateIndex = computed(() => {
-	const d = new Date(props.startDate.setDate(props.startDate.getDate() - props.startDate.getDay()))
-	return new Date(d.setDate(d.getDate() + 1)).getDate()
-})
+  const d = new Date(
+    props.startDate.setDate(
+      props.startDate.getDate() - props.startDate.getDay()
+    )
+  );
+  return new Date(d.setDate(d.getDate() + 1)).getDate();
+});
 const startOfMonth = computed(() => {
-	return new Date(props.startDate.getFullYear(), props.startDate.getMonth(), 1)
-})
+  return new Date(props.startDate.getFullYear(), props.startDate.getMonth(), 1);
+});
 const numberOfDaysInMonth = computed(() => {
-	return new Date(props.startDate.getFullYear(), props.startDate.getMonth() + 1, 0).getDate()
-})
-
+  return new Date(
+    props.startDate.getFullYear(),
+    props.startDate.getMonth() + 1,
+    0
+  ).getDate();
+});
 
 const numberOfEmployees = computed(() => {
-	return props.shifts.length
-})
+  return props.shifts.length;
+});
 
 const uShifts = computed(() => {
-	return props.shifts.flat(1)
-})
-
-
+  return props.shifts.flat(1);
+});
 
 const getShift = (i, ei) => {
-	const value = (i - (startDateIndex.value - (ei * 7))) % uShifts.value.length
-	if (value >= 0) {
-		return uShifts.value[value]
-	} else {
-		return uShifts.value[uShifts.value.length + value]
-	}
-
-
-}
+  const value = (i - (startDateIndex.value - ei * 7)) % uShifts.value.length;
+  if (value >= 0) {
+    return uShifts.value[value];
+  } else {
+    return uShifts.value[uShifts.value.length + value];
+  }
+};
 
 const computedShifts = computed(() => {
-	return Array(numberOfEmployees.value).fill(0).map((_, i) => {
-		return Array(numberOfDaysInMonth.value).fill(0).map((_, j) => {
-			return getShift(j + 1, i)
-		})
-	})
-})
+  return Array(numberOfEmployees.value)
+    .fill(0)
+    .map((_, i) => {
+      return Array(numberOfDaysInMonth.value)
+        .fill(0)
+        .map((_, j) => {
+          return getShift(j + 1, i);
+        });
+    });
+});
 
 const computedReals = computed(() => {
-	return Array(numberOfEmployees.value).fill(0).map((_, i) => {
-		return Array(numberOfDaysInMonth.value).fill(0).map((_, j) => {
-			if (props.reals[i] && props.reals[i][j].length > 0) {
-				const r = props.reals[i][j].map((r) => {
-					return r.type
-				}).join(" + ")
+  return Array(numberOfEmployees.value)
+    .fill(0)
+    .map((_, i) => {
+      return Array(numberOfDaysInMonth.value)
+        .fill(0)
+        .map((_, j) => {
+          if (props.reals[i] && props.reals[i][j].length > 0) {
+            const r = props.reals[i][j]
+              .map((r) => {
+                return r.type;
+              })
+              .join(" + ");
 
-				if (r.includes("L")) {
-					return "L"
-				} else {
-					return r
-				}
-
-
-			}
-			return undefined
-		})
-	})
-})
+            if (r.includes("L")) {
+              return "L";
+            } else {
+              return r;
+            }
+          }
+          return undefined;
+        });
+    });
+});
 
 const hoursWorked = (arr) => {
-
-	let result = 0;
-	for (let a of arr) {
-		switch (a) {
-			case "D":
-				result += 9;
-				break;
-			case "N":
-				result += 14;
-				break;
-			default:
-				break;
-		}
-	}
-	return result;
-}
+  let result = 0;
+  for (let a of arr) {
+    switch (a) {
+      case "D":
+        result += 9;
+        break;
+      case "N":
+        result += 14;
+        break;
+      default:
+        break;
+    }
+  }
+  return result;
+};
 
 const computedMonthlyHours = computed(() => {
-	return computedShifts.value.map((shift) => {
-		return hoursWorked(shift)
-	})
-})
+  return computedShifts.value.map((shift) => {
+    return hoursWorked(shift);
+  });
+});
 
 const computedRealMonthlyHours = computed(() => {
-	return props.reals.map((real) => {
-		return real.map((r) => {
-			return r.map((s) => {
-				switch (s.type) {
-					case "D":
-						return 9;
-					case "N":
-						return 14;
-					case "L":
-						if (s.shift_type === "D") {
-							return -9;
-						} else if (s.shift_type === "N") {
-							return -14;
-						} else {
-							return 0;
-						}
-					default:
-						return 0;
-				}
-			}).reduce((a, b) => a + b, 0)
-		}).reduce((a, b) => a + b, 0)
-	})
-})
-
+  return props.reals.map((real) => {
+    return real
+      .map((r) => {
+        return r
+          .map((s) => {
+            switch (s.type) {
+              case "D":
+                return 9;
+              case "N":
+                return 14;
+              case "L":
+                if (s.shift_type === "D") {
+                  return -9;
+                } else if (s.shift_type === "N") {
+                  return -14;
+                } else {
+                  return 0;
+                }
+              default:
+                return 0;
+            }
+          })
+          .reduce((a, b) => a + b, 0);
+      })
+      .reduce((a, b) => a + b, 0);
+  });
+});
 
 const activeWorking = (index, shift) => {
-	let d = 0;
-	let n = 0;
-	for (const element of shift) {
-		const s = element;
-		switch (s[index - 1]) {
-			case "D":
-				d++;
-				break
-			case "N":
-				n++;
-				break;
-			default:
-				break;
-		}
-	}
-	return `${d}D ${n}N T:${d + n}`;
-}
+  let d = 0;
+  let n = 0;
+  for (const element of shift) {
+    const s = element;
+    switch (s[index - 1]) {
+      case "D":
+        d++;
+        break;
+      case "N":
+        n++;
+        break;
+      default:
+        break;
+    }
+  }
+  return `${d}D ${n}N T:${d + n}`;
+};
 
 const activeRealWorking = (index, shift) => {
-	let d = 0;
-	let n = 0;
-	for (const element of shift) {
-		const s = element[index - 1];
-		for (const shift of s) {
-			switch (shift.type) {
-				case "D":
-					d++;
-					break
-				case "N":
-					n++;
-					break;
-				case "L":
-					if (shift.shift_type === "D") {
-						d--;
-					} else if (shift.shift_type === "N") {
-						n--;
-					}
-					break;
-				default:
-					break;
-			}
-		}
-	}
-	return `${d}D ${n}N T:${d + n}`;
-}
+  let d = 0;
+  let n = 0;
+  for (const element of shift) {
+    const s = element[index - 1];
+    for (const shift of s) {
+      switch (shift.type) {
+        case "D":
+          d++;
+          break;
+        case "N":
+          n++;
+          break;
+        case "L":
+          if (shift.shift_type === "D") {
+            d--;
+          } else if (shift.shift_type === "N") {
+            n--;
+          }
+          break;
+        default:
+          break;
+      }
+    }
+  }
+  return `${d}D ${n}N T:${d + n}`;
+};
+
+onMounted(() => {
+  fetchShifts();
+});
+const depShifts = ref([]);
+const shiftsMap = ref({});
+const fetchShifts = () => {
+  depShifts.value = [];
+  console.log("fetching .....");
+  console.log(props.department);
+
+  //fetch shifts
+  //   frappe.db
+  //     .get_list("Shift Type", {
+  //       filters: { department: props.shiftDeparment },
+  //       fields: ["*"],
+  //       limit: 0,
+  //     })
+  //     .then((res) => {
+  //       depShifts.value = res;
+  //       shiftsMap.value = res.reduce((acc, shift) => {
+  //         acc[shift.shift_suffix] = shift;
+  //         return acc;
+  //       }, {});
+  //     });
+};
 </script>
 <template>
-	<h2 class="mx-auto text-center py-4">{{ moment(startOfMonth).format("MMM YYYY") }}</h2>
-	<div class="w-100 table-responsive">
+  <h2 class="mx-auto text-center py-4">
+    {{ moment(startOfMonth).format("MMM YYYY") }}
+  </h2>
+  <div class="w-100 table-responsive">
+    <table class="table table-borderless table-sticky w-auto">
+      <thead>
+        <tr class="border">
+          <th scope="col" class="text-nowrap">Employee Name</th>
+          <th scope="col" class="text-nowrap">Monthly hrs</th>
+          <th
+            v-for="i in numberOfDaysInMonth"
+            scope="col"
+            class="text-center text-sm"
+            :class="{
+              'bg-danger text-white': startDateIndex === i,
+            }"
+          >
+            {{ i }}
+          </th>
+        </tr>
+        <tr class="border">
+          <th scope="col" class="text-nowrap">Day</th>
+          <th scope="col" class="text-nowrap"></th>
+          <th
+            v-for="i in numberOfDaysInMonth"
+            scope="col"
+            class="px-4"
+            :class="{
+              'bg-danger text-white': startDateIndex === i,
+            }"
+          >
+            {{
+              moment(startOfMonth)
+                .add(i - 1, "d")
+                .format("ddd")
+            }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <template v-for="(e, ei) in numberOfEmployees">
+          <tr class="border-top">
+            <th scope="row" class="text-nowrap border-left">
+              {{ props.employees[ei].employee_name }}
+            </th>
+            <td scope="row" class="text-center">
+              {{ computedMonthlyHours[ei] }}
+            </td>
+            <td
+              v-for="i in numberOfDaysInMonth"
+              class="text-center"
+              :class="{
+                'bg-red-100': computedShifts[ei][i - 1] === 'R',
+                'bg-green-100': computedShifts[ei][i - 1] === 'D',
+                'bg-green-300': computedShifts[ei][i - 1] === 'N',
+              }"
+            >
+              {{ computedShifts[ei][i - 1] }}
+            </td>
+          </tr>
+          <tr>
+            <td class="text-nowrap border-left">
+              {{ props.employees[ei].name }}
+            </td>
+            <td
+              class="text-center"
+              :class="{
+                'bg-danger text-white':
+                  computedMonthlyHours[ei] > computedRealMonthlyHours[ei],
+                'bg-success text-white':
+                  computedMonthlyHours[ei] < computedRealMonthlyHours[ei],
+              }"
+            >
+              {{ computedRealMonthlyHours[ei] }}
+            </td>
+            <td
+              v-for="i in numberOfDaysInMonth"
+              class="text-center"
+              :class="{
+                'bg-danger text-white': computedReals[ei][i - 1] === 'L',
+                'bg-success text-white':
+                  computedReals[ei][i - 1] &&
+                  computedReals[ei][i - 1].includes('+'),
+                // 'bg-green-100': computedReals[ei][i - 1] === 'D',
+                // 'bg-green-300': computedReals[ei][i - 1] === 'N'
+              }"
+            >
+              {{ computedReals[ei][i - 1] }}
+            </td>
+          </tr>
+          <tr>
+            <th scope="row" class="text-nowrap border-left"></th>
+            <td scope="row" class="text-center"></td>
+            <td v-for="i in numberOfDaysInMonth" class="text-center"></td>
+          </tr>
+        </template>
 
-		<table class="table table-borderless table-sticky  w-auto">
-			<thead>
-				<tr class="border">
-					<th scope="col" class="text-nowrap">Employee Name </th>
-					<th scope="col" class="text-nowrap">Monthly hrs</th>
-					<th v-for="i in numberOfDaysInMonth" scope="col" class="text-center text-sm" :class="{
-						'bg-danger text-white': startDateIndex === i
-					}">{{ i }}</th>
-				</tr>
-				<tr class="border">
-					<th scope="col" class="text-nowrap">Day </th>
-					<th scope="col" class="text-nowrap"></th>
-					<th v-for="i in numberOfDaysInMonth" scope="col" class="px-4" :class="{
-						'bg-danger text-white': startDateIndex === i
-					}">{{ moment(startOfMonth).add(i - 1, 'd').format("ddd") }}
-					</th>
-				</tr>
-			</thead>
-			<tbody>
-				<template v-for="(e, ei) in numberOfEmployees">
-					<tr class="border-top">
+        <tr class="bg-orange-100">
+          <td
+            class="text-center border whitespace-nowrap text-sm font-medium text-gray-800"
+          ></td>
+          <td
+            class="text-center border whitespace-nowrap text-sm font-medium text-gray-800"
+          ></td>
+          <td
+            v-for="i in numberOfDaysInMonth"
+            class="border text-center whitespace-nowrap text-sm text-gray-800 p-2"
+          >
+            <div>
+              <div>{{ activeWorking(i, computedShifts) }}</div>
+            </div>
+          </td>
 
-						<th scope="row" class="text-nowrap border-left">{{ props.employees[ei].employee_name }}</th>
-						<td scope="row" class="text-center">{{ computedMonthlyHours[ei] }}</td>
-						<td v-for="i in numberOfDaysInMonth" class="text-center" :class="{
-							'bg-red-100': computedShifts[ei][i - 1] === 'R',
-							'bg-green-100': computedShifts[ei][i - 1] === 'D',
-							'bg-green-300': computedShifts[ei][i - 1] === 'N'
-						}">{{ computedShifts[ei][i - 1] }}</td>
-					</tr>
-					<tr>
+          <td class="whitespace-nowrap text-end text-sm font-medium"></td>
+        </tr>
+        <tr class="bg-orange-100">
+          <td
+            class="text-center border whitespace-nowrap text-sm font-medium text-gray-800"
+          ></td>
+          <td
+            class="text-center border whitespace-nowrap text-sm font-medium text-gray-800"
+          ></td>
+          <td
+            v-for="i in numberOfDaysInMonth"
+            class="border text-center whitespace-nowrap text-sm text-gray-800 p-2"
+            :class="{
+              'bg-warning text-black':
+                activeWorking(i, computedShifts) !==
+                activeRealWorking(i, props.reals),
+            }"
+          >
+            <div>
+              <div>{{ activeRealWorking(i, props.reals) }}</div>
+            </div>
+          </td>
 
-						<td class="text-nowrap border-left">{{ props.employees[ei].name }}</td>
-						<td class="text-center" :class="{
-							'bg-danger text-white': computedMonthlyHours[ei] > computedRealMonthlyHours[ei],
-							'bg-success text-white': computedMonthlyHours[ei] < computedRealMonthlyHours[ei],
-						}">{{ computedRealMonthlyHours[ei] }}</td>
-						<td v-for="i in numberOfDaysInMonth" class="text-center" :class="{
-							'bg-danger text-white': computedReals[ei][i - 1] === 'L',
-							'bg-success text-white': computedReals[ei][i - 1] && computedReals[ei][i - 1].includes('+'),
-							// 'bg-green-100': computedReals[ei][i - 1] === 'D',
-							// 'bg-green-300': computedReals[ei][i - 1] === 'N'
-						}">{{ computedReals[ei][i - 1]
-							}}</td>
-					</tr>
-					<tr>
-
-						<th scope="row" class="text-nowrap border-left"></th>
-						<td scope="row" class="text-center"></td>
-						<td v-for="i in numberOfDaysInMonth" class="text-center"></td>
-					</tr>
-				</template>
-
-
-				<tr class="bg-orange-100">
-					<td class="  text-center border whitespace-nowrap text-sm font-medium text-gray-800">
-					</td>
-					<td class="  text-center border whitespace-nowrap text-sm font-medium text-gray-800">
-					</td>
-					<td v-for="i in numberOfDaysInMonth"
-						class="border text-center whitespace-nowrap text-sm text-gray-800 p-2">
-
-						<div>
-							<div> {{ activeWorking(i, computedShifts) }}</div>
-						</div>
-					</td>
-
-					<td class=" whitespace-nowrap text-end text-sm font-medium">
-
-					</td>
-				</tr>
-				<tr class="bg-orange-100">
-					<td class="  text-center border whitespace-nowrap text-sm font-medium text-gray-800">
-					</td>
-					<td class="  text-center border whitespace-nowrap text-sm font-medium text-gray-800">
-					</td>
-					<td v-for="i in numberOfDaysInMonth"
-						class="border text-center whitespace-nowrap text-sm text-gray-800 p-2" :class="{
-							'bg-warning text-black': activeWorking(i, computedShifts) !== activeRealWorking(i, props.reals)
-						}">
-						<div>
-							<div> {{ activeRealWorking(i, props.reals) }}</div>
-						</div>
-					</td>
-
-					<td class=" whitespace-nowrap text-end text-sm font-medium">
-
-					</td>
-				</tr>
-
-			</tbody>
-		</table>
-
-	</div>
+          <td class="whitespace-nowrap text-end text-sm font-medium"></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <style>
 .bg-red-100 {
-	background-color: #FEE2E2;
+  background-color: #fee2e2;
 }
 
 .bg-blue-100 {
-	background-color: #DBEAFE;
+  background-color: #dbeafe;
 }
 
 .bg-green-100 {
-	background-color: #F0FFF4;
+  background-color: #f0fff4;
 }
 
 .bg-green-300 {
-	background-color: #C6F6D5;
+  background-color: #c6f6d5;
 }
 
 .table-sticky th:first-child,
 .table-sticky td:first-child {
-	position: sticky;
-	left: 0;
-	background-color: white;
+  position: sticky;
+  left: 0;
+  background-color: white;
 }
 </style>
