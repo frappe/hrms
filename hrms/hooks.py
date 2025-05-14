9386520d90b8f4770421196801389a566e021126
karianyu <88138535+karianyu@users.noreply.github.com>
2025-05-14 09:49:25 +0300

    Update hooks.py

--- hrms/hooks.py
+++ hrms/hooks.py
@@ -4,7 +4,7 @@ app_publisher = "Frappe Technologies Pvt. Ltd."
 app_description = "Modern HR and Payroll Software"
 app_email = "contact@frappe.io"
 app_license = "GNU General Public License (v3)"
-required_apps = ["frappe/erpnext"]
+#required_apps = ["frappe/erpnext"]
 source_link = "http://github.com/frappe/hrms"
 
 add_to_apps_screen = [
