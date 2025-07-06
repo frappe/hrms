dfe90658710b3a2b78e7a13d2a1e5560635a7443
thomasantony12 <thomasantony12042001@gmail.com>
2025-07-06 14:14:42 +0530

    feat: Allow employees to reopen grievance

--- hrms/hr/doctype/employee_grievance/employee_grievance.js
+++ hrms/hr/doctype/employee_grievance/employee_grievance.js
@@ -2,6 +2,48 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Employee Grievance", {
+	refresh: (frm) => {
+		if (
+			frm.doc.owner === frappe.session.user &&
+			frm.doc.docstatus === 1 &&
+			!frm.doc.against_employee_grievance
+		) {
+			frm.add_custom_button(__("Re-open"), () => {
+				let d = new frappe.ui.Dialog({
+					title: "Reason for re-open",
+					fields: [
+						{
+							label: "Reason",
+							fieldname: "reason",
+							fieldtype: "Small Text",
+							reqd: 1,
+						},
+					],
+					size: "large",
+					primary_action_label: "Re-open",
+					primary_action(values) {
+						frappe.model.with_doctype("Employee Grievance", () => {
+							let employeeGrievance = frappe.model.get_new_doc("Employee Grievance");
+							let fields = Object.keys(frm.doc);
+							let fieldsToExclude = ["name", "docstatus"];
+
+							fields.forEach((field) => {
+								if (fieldsToExclude.includes(field)) return;
+								employeeGrievance[field] = frm.doc[field];
+							});
+							employeeGrievance.against_employee_grievance = frm.doc.name;
+							employeeGrievance.reopen_reason = values.reason;
+							employeeGrievance.status = "Open";
+							employeeGrievance.date = frappe.datetime.get_today();
+
+							frappe.set_route("Form", "Employee Grievance", employeeGrievance.name);
+						});
+					},
+				});
+				d.show();
+			});
+		}
+	},
 	setup: function (frm) {
 		frm.set_query("grievance_against_party", function () {
 			return {
