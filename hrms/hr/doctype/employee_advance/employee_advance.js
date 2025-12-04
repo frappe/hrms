a4c5f10b1cf867093e94c83197bb970155214cf0
El-Shafei H. <el.shafei.developer@gmail.com>
2025-12-04 09:14:17 +0300

    fix: remove filter query for non-existent fields

--- hrms/hr/doctype/employee_advance/employee_advance.js
+++ hrms/hr/doctype/employee_advance/employee_advance.js
@@ -26,13 +26,6 @@ frappe.ui.form.on("Employee Advance", {
 			};
 		});
 
-		frm.set_query("salary_component", function () {
-			return {
-				filters: {
-					type: "Deduction",
-				},
-			};
-		});
 	},
 
 	refresh: function (frm) {
