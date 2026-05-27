6f8d1c1321f4c4efb7954af50e41763c64912337
Asmita Hase <asmitahase@gmail.com>
2026-05-27 10:34:57 +0530

    feat(minor): description field for exemption categories and sub categories.
    (cherry picked from commit dee44d5b3564248024e502f5a5907c0896c75cca)
    
    # Conflicts:
    #	hrms/payroll/doctype/employee_tax_exemption_category/employee_tax_exemption_category.py
    #	hrms/payroll/doctype/employee_tax_exemption_sub_category/employee_tax_exemption_sub_category.py

--- hrms/payroll/doctype/employee_tax_exemption_category/employee_tax_exemption_category.py
+++ hrms/payroll/doctype/employee_tax_exemption_category/employee_tax_exemption_category.py
@@ -6,4 +6,20 @@ from frappe.model.document import Document
 
 
 class EmployeeTaxExemptionCategory(Document):
+<<<<<<< HEAD
+=======
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		description: DF.SmallText | None
+		is_active: DF.Check
+		max_amount: DF.Currency
+	# end: auto-generated types
+
+>>>>>>> dee44d5b3 (feat(minor): description field for exemption categories and sub categories.)
 	pass
--- hrms/payroll/doctype/employee_tax_exemption_sub_category/employee_tax_exemption_sub_category.py
+++ hrms/payroll/doctype/employee_tax_exemption_sub_category/employee_tax_exemption_sub_category.py
@@ -9,6 +9,23 @@ from frappe.utils import flt
 
 
 class EmployeeTaxExemptionSubCategory(Document):
+<<<<<<< HEAD
+=======
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		description: DF.SmallText | None
+		exemption_category: DF.Link
+		is_active: DF.Check
+		max_amount: DF.Currency
+	# end: auto-generated types
+
+>>>>>>> dee44d5b3 (feat(minor): description field for exemption categories and sub categories.)
 	def validate(self):
 		category_max_amount = frappe.db.get_value(
 			"Employee Tax Exemption Category", self.exemption_category, "max_amount"
