f3a99a3d5ac37ccd794f23bc215ecaa4447e7bf0
Deepesh Garg <deepeshgarg6@gmail.com>
2026-06-01 16:08:35 +0530

    chore: resolve conflicts

--- hrms/payroll/doctype/salary_structure/salary_structure.py
+++ hrms/payroll/doctype/salary_structure/salary_structure.py
@@ -357,7 +357,6 @@ def get_existing_assignments(employees, salary_structure, from_date):
 
 @frappe.whitelist()
 def make_salary_slip(
-<<<<<<< HEAD
 	source_name,
 	target_doc=None,
 	employee=None,
@@ -365,19 +364,7 @@ def make_salary_slip(
 	as_print=False,
 	print_format=None,
 	for_preview=0,
-	ignore_permissions=False,
 ):
-=======
-	source_name: str,
-	target_doc: str | Document | None = None,
-	employee: str | None = None,
-	posting_date: str | datetime.date | None = None,
-	as_print: bool = False,
-	print_format: str | None = None,
-	for_preview: int = 0,
-	lwp_days_corrected: float | None = None,
-) -> str | Document:
->>>>>>> 9b4f3a344 (fix: Better perm checks for HRMS)
 	def postprocess(source, target):
 		if employee:
 			target.employee = employee
