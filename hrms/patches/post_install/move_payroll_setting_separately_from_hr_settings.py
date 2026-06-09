38d42952f96ca792a20e17d4a8a525483bfcc533
Sahil <sahilsuthar607@gmail.com>
2026-06-09 16:57:44 +0530

    refactor: optimize database query for payroll settings migration
    (cherry picked from commit ffc2a78fe77c88c0e988d357c5043471adcbb89b)

--- hrms/patches/post_install/move_payroll_setting_separately_from_hr_settings.py
+++ hrms/patches/post_install/move_payroll_setting_separately_from_hr_settings.py
@@ -3,27 +3,30 @@
 
 
 import frappe
+from frappe.query_builder import DocType
 
 
 def execute():
-	data = frappe.db.sql(
-		"""SELECT *
-        FROM `tabSingles`
-        WHERE
-            doctype = "HR Settings"
-        AND
-            field in (
-                "encrypt_salary_slips_in_emails",
-                "email_salary_slip_to_employee",
-                "daily_wages_fraction_for_half_day",
-                "disable_rounded_total",
-                "include_holidays_in_total_working_days",
-                "max_working_hours_against_timesheet",
-                "payroll_based_on",
-                "password_policy"
-            )
-            """,
-		as_dict=1,
+	singles = DocType("Singles")
+	data = (
+		frappe.qb.from_(singles)
+		.select("*")
+		.where(singles.doctype == "HR Settings")
+		.where(
+			singles.field.isin(
+				[
+					"encrypt_salary_slips_in_emails",
+					"email_salary_slip_to_employee",
+					"daily_wages_fraction_for_half_day",
+					"disable_rounded_total",
+					"include_holidays_in_total_working_days",
+					"max_working_hours_against_timesheet",
+					"payroll_based_on",
+					"password_policy",
+				]
+			)
+		)
+		.run(as_dict=True)
 	)
 
 	for d in data:
