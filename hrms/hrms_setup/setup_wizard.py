8c670c543a73c6028ed40d3724a7c3e5cccc00e7
Krishna Shirsath <shirsathkrishna19@gmail.com>
2026-06-16 12:38:07 +0530

    refactor(setup): move demo stages appending inside conditional check

--- hrms/hrms_setup/setup_wizard.py
+++ hrms/hrms_setup/setup_wizard.py
@@ -10,22 +10,25 @@ from hrms.hrms_setup.demo import create_demo_company, setup_demo_data
 def get_setup_stages(args=None):
 	stages = []
 
-	stages.append(
-		{
-			"status": _("Creating Demo Company and Organization"),
-			"fail_msg": _("Failed to create Demo Company and Organization"),
-			"tasks": [
-				{"fn": create_demo_company, "args": args, "fail_msg": _("Failed to create Demo Company")}
-			],
-		}
-	)
+	if args.get("setup_demo"):
+		stages.append(
+			{
+				"status": _("Creating Demo Company and Organization"),
+				"fail_msg": _("Failed to create Demo Company and Organization"),
+				"tasks": [
+					{"fn": create_demo_company, "args": args, "fail_msg": _("Failed to create Demo Company")}
+				],
+			}
+		)
 
-	stages.append(
-		{
-			"status": _("Creating HR Demo Data"),
-			"fail_msg": _("Failed to create HR Demo Data"),
-			"tasks": [{"fn": setup_demo_data, "args": args, "fail_msg": _("Failed to create HR Demo Data")}],
-		}
-	)
+		stages.append(
+			{
+				"status": _("Creating HR Demo Data"),
+				"fail_msg": _("Failed to create HR Demo Data"),
+				"tasks": [
+					{"fn": setup_demo_data, "args": args, "fail_msg": _("Failed to create HR Demo Data")}
+				],
+			}
+		)
 
 	return stages
