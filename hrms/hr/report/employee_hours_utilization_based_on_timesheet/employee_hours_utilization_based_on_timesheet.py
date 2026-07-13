2344f6fd8fdd34c4bd3d529e69006ab690d8d363
nareshkannasln <nareshkannashanmugam@gmail.com>
2026-07-13 12:09:28 +0530

    perf: update db.get_value to get_all
    (cherry picked from commit feb07e9116d6a94db20dd03a25eadb5433759053)

--- hrms/hr/report/employee_hours_utilization_based_on_timesheet/employee_hours_utilization_based_on_timesheet.py
+++ hrms/hr/report/employee_hours_utilization_based_on_timesheet/employee_hours_utilization_based_on_timesheet.py
@@ -166,12 +166,20 @@ class EmployeeHoursReport:
 				self.stats_by_employee[emp]["non_billed_hours"] += flt(hours, 2)
 
 	def set_employee_department_and_name(self):
-		for emp in self.stats_by_employee:
-			emp_name = frappe.db.get_value("Employee", emp, "employee_name")
-			emp_dept = frappe.db.get_value("Employee", emp, "department")
+		if not self.stats_by_employee:
+			return
 
-			self.stats_by_employee[emp]["department"] = emp_dept
-			self.stats_by_employee[emp]["employee_name"] = emp_name
+		employee_details = frappe.get_all(
+			"Employee",
+			filters={"name": ("in", list(self.stats_by_employee))},
+			fields=["name", "employee_name", "department"],
+		)
+		details_by_employee = {d.name: d for d in employee_details}
+
+		for emp, stats in self.stats_by_employee.items():
+			details = details_by_employee.get(emp, frappe._dict())
+			stats["department"] = details.get("department")
+			stats["employee_name"] = details.get("employee_name")
 
 	def calculate_utilizations(self):
 		TOTAL_HOURS = flt(self.standard_working_hours * self.day_span, 2)
