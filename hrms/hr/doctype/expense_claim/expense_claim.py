a2f6804e0f1e8421f764c2ff82596c16b900143e
nareshkannasln <nareshkannashanmugam@gmail.com>
2026-07-15 21:07:42 +0530

    chore: remove validation

--- hrms/hr/doctype/expense_claim/expense_claim.py
+++ hrms/hr/doctype/expense_claim/expense_claim.py
@@ -119,9 +119,6 @@ class ExpenseClaim(AccountsController, PWANotificationsMixin):
 		if flt(self.grand_total) > 0 and self.total_advance_amount:
 			self.is_paid = 0
 
-		if not self.payable_account and not self.is_paid:
-			frappe.throw(_("Payable Account is mandatory to submit an Expense Claim"))
-
 	def set_status(self, update=False):
 		status = {"0": "Draft", "1": "Submitted", "2": "Cancelled"}[cstr(self.docstatus or 0)]
 
