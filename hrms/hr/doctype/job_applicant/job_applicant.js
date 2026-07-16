2208a0137333fd0dbaab6ae68c79dca6bccac852
iamkhanraheel <raheel@frappe.io>
2026-07-16 16:09:28 +0530

    fix: group interview timing fields in schedule interview dialog

--- hrms/hr/doctype/job_applicant/job_applicant.js
+++ hrms/hr/doctype/job_applicant/job_applicant.js
@@ -155,6 +155,12 @@ frappe.ui.form.on("Job Applicant", {
 						});
 					},
 				},
+				{
+					label: __("From Time"),
+					fieldname: "from_time",
+					fieldtype: "Time",
+					reqd: 1,
+				},
 				{ fieldtype: "Column Break" },
 				{
 					label: __("Scheduled On"),
@@ -163,14 +169,6 @@ frappe.ui.form.on("Job Applicant", {
 					reqd: 1,
 					default: frappe.datetime.get_today(),
 				},
-				{ fieldtype: "Section Break" },
-				{
-					label: __("From Time"),
-					fieldname: "from_time",
-					fieldtype: "Time",
-					reqd: 1,
-				},
-				{ fieldtype: "Column Break" },
 				{
 					label: __("To Time"),
 					fieldname: "to_time",
@@ -185,7 +183,6 @@ frappe.ui.form.on("Job Applicant", {
 					fieldname: "interviewers",
 					fieldtype: "Table MultiSelect",
 					options: "Interview Detail",
-					label: __("Interviewers"),
 				},
 			],
 			primary_action_label: __("Schedule"),
