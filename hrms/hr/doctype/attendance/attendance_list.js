af74e00f23d1bc2a8af32b74c05b9666001a0b4e
nareshkannasln <nareshkannashanmugam@gmail.com>
2026-07-16 18:09:44 +0530

    fix: use date strings for mark attendance date

--- hrms/hr/doctype/attendance/attendance_list.js
+++ hrms/hr/doctype/attendance/attendance_list.js
@@ -46,7 +46,7 @@ frappe.listview_settings["Attendance"] = {
 							fieldtype: "Date",
 							fieldname: "from_date",
 							reqd: 1,
-							default: first_day_of_month.toDate(),
+							default: first_day_of_month.format("YYYY-MM-DD"),
 							onchange: () => me.get_unmarked_days(dialog),
 						},
 						{
@@ -65,7 +65,7 @@ frappe.listview_settings["Attendance"] = {
 							fieldtype: "Date",
 							fieldname: "to_date",
 							reqd: 1,
-							default: moment().toDate(),
+							default: moment().format("YYYY-MM-DD"),
 							onchange: () => me.get_unmarked_days(dialog),
 						},
 						{
