27336f6cef902b18aa3c16676215677ff0ca9df5
Deepesh Garg <deepeshgarg6@gmail.com>
2026-08-14 12:00:15 +0530

    fix: telemetry condition

---
+++ hrms/hr/doctype/hr_telemetry_milestone/hr_telemetry_milestone.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+
+class HRTelemetryMilestone(Document):
+	pass
