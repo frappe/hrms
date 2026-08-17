6d5137ece0509666ae833b2dadec15075434644b
pratheep-bit <pratheeps2024@gmail.com>
2026-08-17 09:43:40 +0530

    fix(hr): exclude cancelled Job Offers in JobApplicant.onload

--- hrms/hr/doctype/job_applicant/job_applicant.py
+++ hrms/hr/doctype/job_applicant/job_applicant.py
@@ -49,9 +49,9 @@ class JobApplicant(Document):
 	# end: auto-generated types
 
 	def onload(self):
-		job_offer = frappe.get_all("Job Offer", filters={"job_applicant": self.name})
+		job_offer = frappe.get_all("Job Offer", filters={"job_applicant": self.name, "docstatus": ["!=", 2]})
 		if job_offer:
-			self.get("__onload").job_offer = job_offer[0].name
+			self.set_onload("job_offer", job_offer[0].name)
 
 	def autoname(self):
 		self.name = self.email_id
