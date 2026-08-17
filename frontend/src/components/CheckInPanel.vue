f1a43002b40f944760e3fc5d9faeda40b9e64fca
Raji <raji.frappe@gmail.com>
2026-08-17 10:41:35 +0000

    fix: translate View List in check-in panel

--- frontend/src/components/CheckInPanel.vue
+++ frontend/src/components/CheckInPanel.vue
@@ -9,7 +9,7 @@
 				<span>{{ __("Last {0} was at {1}", [__(lastLogType), formatTimestamp(lastLog.time)]) }}</span>
 				<span class="whitespace-pre"> &middot; </span>
 				<router-link :to="{ name: 'EmployeeCheckinListView' }" v-slot="{ navigate }">
-					<span @click="navigate" class="underline">View List</span>
+					<span @click="navigate" class="underline">{{ __("View List") }}</span>
 				</router-link>
 			</div>
 			<Button
