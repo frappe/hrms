d10bf903c19660645d920450afdb05bdf1223ae8
Deepesh Garg <deepeshgarg6@gmail.com>
2026-09-09 11:17:06 +0530

    chore: linting issues

--- hrms/hr/report/employee_leave_balance/employee_leave_balance.js
+++ hrms/hr/report/employee_leave_balance/employee_leave_balance.js
@@ -84,7 +84,8 @@ frappe.query_reports["Employee Leave Balance"] = {
 			},
 		});
 
-		const is_restricted_to_specific_employees = frappe.defaults.get_user_permissions()["Employee"];
+		const is_restricted_to_specific_employees = 
+			frappe.defaults.get_user_permissions()["Employee"];
 		if (is_restricted_to_specific_employees) {
 			hrms.get_current_employee().then((employee) => {
 				if (employee) {
